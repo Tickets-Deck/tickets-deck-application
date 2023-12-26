2699f64551104eabec1ea6123af9a47769ba7dc8
Simlex <simlexafol@gmail.com>
2023-12-26 01:31:54 +0100

    Created session type

---
+++ app/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            id: string | null | undefined;
+            name: string | null | undefined;
+            email: string | null | undefined;
+            image: string | null | undefined;
+            username: string | null | undefined;
+            idToken: string | null | undefined;
+            accessToken: string | null | undefined;
+        }
+    }
+}
