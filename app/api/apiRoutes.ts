3878d5137ec4f5430079c61efce89130e47f59a3
Simlex <simlexafol@gmail.com>
2023-12-26 01:28:48 +0100

    Add API routes and endpoints

---
+++ app/api/apiRoutes.ts
@@ -0,0 +1,14 @@
+/**
+ * The API routes endpoints
+ */
+export class ApiRoutes {
+  /**
+   * The dev base url for the application
+   */
+  static BASE_URL_DEV: string = "http://localhost:9000/";
+
+  /**
+   * The route to CreateNewsletterSubscriber endpoint
+   */
+  static CreateNewsletterSubscriber: string = "api/newsletter-subscriber";
+}
