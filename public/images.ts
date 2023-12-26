b1fea6d0b176c6489d3d0fe4cb402473a2612ad1
Simlex <simlexafol@gmail.com>
2023-12-26 01:22:01 +0100

    Added images

---
+++ public/images.ts
@@ -0,0 +1,43 @@
+import logoWhite from "./images/logo_white.png";
+import logoPurple from "./images/logo_purple.png";
+// import ImageBg7 from "./images/ImageBg7.jpg";
+// import ImageBg8 from "./images/ImageBg8.jpg";
+import rocket from "./images/rocket.png";
+import ticketbg from "./images/ticketbg.png";
+import event_flyer from "./images/event_flyer.jpg";
+import createImage from "./images/createImage.png";
+import createImageMobile from "./images/createImageMobile.png";
+import woman_dancing from "./images/woman_dancing.svg";
+import user_avatar from "./images/user_avatar.png";
+import sad_face from "./images/sad_face.png";
+import fingers_crossed from "./images/fingers_crossed.png";
+
+
+const ImageBg1 = "https://res.cloudinary.com/dyxqhvkzp/image/upload/v1701140925/hero_images/ImageBg1_nnev1u.jpg";
+const ImageBg2 = "https://res.cloudinary.com/dyxqhvkzp/image/upload/v1701140882/hero_images/ImageBg2_wk6w74.jpg";
+const ImageBg3 = "https://res.cloudinary.com/dyxqhvkzp/image/upload/v1701140935/hero_images/ImageBg3_j0fq4y.jpg";
+const ImageBg4 = "https://res.cloudinary.com/dyxqhvkzp/image/upload/v1701140884/hero_images/ImageBg4_xq1gcm.jpg";
+const ImageBg5 = "https://res.cloudinary.com/dyxqhvkzp/image/upload/v1701140920/hero_images/ImageBg5_jkbucy.jpg";
+const ImageBg6 = "https://res.cloudinary.com/dyxqhvkzp/image/upload/v1701141965/hero_images/ImageBg6_e2jb97.jpg";
+
+export default {
+  logoWhite,
+  logoPurple,
+  ImageBg1,
+  ImageBg2,
+  ImageBg3,
+  ImageBg4,
+  ImageBg5,
+  ImageBg6,
+//   ImageBg7,
+//   ImageBg8,
+  rocket,
+  ticketbg,
+  event_flyer,
+  createImage,
+  createImageMobile,
+  woman_dancing,
+  user_avatar,
+  sad_face,
+  fingers_crossed
+};
