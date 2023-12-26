16ea54f08596be6573d4907ac0838edf3fc26a3a
Simlex <simlexafol@gmail.com>
2023-12-26 01:28:41 +0100

    Add API client and createNewsletterSubscriber function

---
+++ app/api/apiClient.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { ApiRoutes } from "./apiRoutes";
+import { UserCredentialsRequest } from "../components/models/IUser";
+
+export const API = axios.create({
+  baseURL: ApiRoutes.BASE_URL_DEV,
+});
+
+export function useCreateNewsletterSubscriber() {
+  async function createNewsletterSubscriber(email: string) {
+    return API.post(ApiRoutes.CreateNewsletterSubscriber, { email });
+  }
+
+  return createNewsletterSubscriber;
+}
