84205686563167a1ddbf6fda1aa0a1b473006176
Simlex <simlexafol@gmail.com>
2023-12-28 09:06:12 +0100

    Add authentication middleware to redirect unauthorized users

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,23 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/app")) {
-    // This logic is only applied to pages that start with /app
-    return NextResponse.redirect(new URL("/auth/signin", request.url));
-  } else {
-    // Otherwise, let the request continue
-    return NextResponse.next();
-  }
+  const userIsAuthenticated = request.cookies.get("next-auth.session-token");
+
+    // If the user is not authenticated, redirect to /auth/signin
+    if (!userIsAuthenticated && request.nextUrl.pathname.startsWith("/app")) {
+        return NextResponse.redirect(new URL("/auth/signin", request.url));
+    } else {
+        // Otherwise, let the request continue
+        return NextResponse.next();
+    }
+
+//   if (request.nextUrl.pathname.startsWith("/app")) {
+//     // This logic is only applied to pages that start with /app
+//     return NextResponse.redirect(new URL("/auth/signin", request.url));
+//   } else {
+//     // Otherwise, let the request continue
+//     return NextResponse.next();
+//   }
 }
 
 // See "Matching Paths" below to learn more
