7e50ce2c96a8151378e3f1ae4617f65f5a692665
Simlex <simlexafol@gmail.com>
2023-12-28 09:08:38 +0100

    Refactor event creation page and add image upload and ticket details sections

--- app/app/event/create/page.tsx
+++ app/app/event/create/page.tsx
@@ -1,8 +1,21 @@
 "use client"
-import React, { FunctionComponent, ReactElement, useState } from "react";
+import React, { FunctionComponent, ReactElement, useCallback, useRef, useState, FormEvent, ChangeEvent, Dispatch, SetStateAction, useEffect } from "react";
 import styles from '../../../styles/CreateEvent.module.scss';
-import { EventRequest } from "../../../components/models/IEventRequest";
 import CreateEventProgressBar from "../../../components/shared/CreateEventProgressBar";
+import { AddEventIcon, AddIcon, CalenderIcon, CloseIcon, PhotoIcon } from "@/app/components/SVGs/SVGicons";
+import { DatePicker, ITimeRange } from "@fluentui/react";
+import { categories } from "@/app/constants/eventCategories";
+import { EventRequest } from "@/app/models/IEvents";
+import useOuterClick from "@/app/hooks/useOuterClick";
+import { EventVisibility } from "@/app/enums/IEventVisibility";
+import BasicInformationForm from "@/app/components/Event/Create/BasicInformationForm";
+import { ValidationStatus } from "@/app/enums/BasicInfoFormValidationStatus";
+import Image from "next/image";
+import images from "@/public/images";
+import { EventCreationStage } from "@/app/enums/EventCreationStage";
+import ImageUploadSection from "@/app/components/Event/Create/ImageUploadSection";
+import { Tickets } from "@/app/models/ITicket";
+import TicketDetailsSection from "@/app/components/Event/Create/TicketDetailsSection";
 
 interface CreateEventProps {
 
@@ -10,7 +23,49 @@ interface CreateEventProps {
 
 const CreateEvent: FunctionComponent<CreateEventProps> = (): ReactElement => {
 
-    const [createEventRequest, setCreateEventRequest] = useState<EventRequest>();
+    const [eventCreationStage, setEventCreationStage] = useState<EventCreationStage>(EventCreationStage.BasicInfo);
+    const [eventRequest, setEventRequest] = useState<EventRequest>();
+    const [validationStage, setValidationStage] = useState<{ status: ValidationStatus }>();
+
+    function proceedToImageUpload() {
+
+        // Set the validation stage to running
+        setValidationStage({ status: ValidationStatus.Running });
+
+        // Set visibility to public if it is undefined
+        if (!eventRequest?.visibility) {
+            setEventRequest({ ...eventRequest as EventRequest, visibility: EventVisibility.PUBLIC });
+        }
+
+        // Update the event creation stage
+        // setEventCreationStage(EventCreationStage.ImageUpload);
+
+        console.log(eventRequest);
+    };
+
+    function moveToNextStage(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        switch (eventCreationStage) {
+            case EventCreationStage.BasicInfo:
+                proceedToImageUpload();
+                break;
+            case EventCreationStage.ImageUpload:
+                setEventCreationStage(EventCreationStage.TicketDetails);
+                break;
+            case EventCreationStage.TicketDetails:
+                setEventCreationStage(EventCreationStage.Confirmation);
+                break;
+            case EventCreationStage.Confirmation:
+                setEventCreationStage(EventCreationStage.BasicInfo);
+                break;
+        }
+    };
+
+    // useEffect(() => {
+    //     console.log(eventRequest);
+    // }, [eventCreationStage]); 
+
 
     return (
         <div className={styles.main}>
@@ -21,52 +76,55 @@ const CreateEvent: FunctionComponent<CreateEventProps> = (): ReactElement => {
                 </Link> */}
             </div>
 
-            <CreateEventProgressBar />
+            <CreateEventProgressBar
+                eventCreationStage={eventCreationStage}
+                setEventCreationStage={setEventCreationStage}
+                eventRequest={eventRequest}
+            />
 
-            <div className={styles.formContainer}>
-                <form>
-                    <div className={styles.formField}>
-                        <label htmlFor="title">Title</label>
-                        <input type="text" name="title" id="title" placeholder="Event Title" />
+            <form onSubmit={moveToNextStage}>
+                {eventCreationStage === EventCreationStage.BasicInfo &&
+                    <BasicInformationForm
+                        eventRequest={eventRequest}
+                        setEventRequest={setEventRequest}
+                        validationStage={validationStage}
+                        setValidationStage={setValidationStage}
+                        setEventCreationStage={setEventCreationStage}
+                    />}
+
+                {eventCreationStage === EventCreationStage.ImageUpload &&
+                    <ImageUploadSection
+                        eventRequest={eventRequest}
+                        setEventRequest={setEventRequest}
+                    />}
+
+                {eventCreationStage === EventCreationStage.TicketDetails &&
+                    <TicketDetailsSection
+                        eventRequest={eventRequest}
+                        setEventRequest={setEventRequest}
+                    />
+                }
+
+                {eventCreationStage === EventCreationStage.Confirmation &&
+                    <div>Confirmation</div>
+                }
+
+                <div className={styles.actionButtons}>
+                    <div className={styles.tagSection}>
+                        {
+                            eventRequest?.tags?.map((tag, index) => {
+                                return (
+                                    <span className={styles.tag} key={index}> 
+                                        {tag}
+                                        <span><CloseIcon /></span>
+                                    </span>
+                                )
+                            })
+                        }
                     </div>
-                    <div className={styles.formField}>
-                        <label htmlFor="description">Description</label>
-                        <textarea name="description" id="description" placeholder="Event Description" />
-                    </div>
-                    <div className={styles.formField}>
-                        <label htmlFor="venue">Location</label>
-                        <input type="text" name="venue" id="venue" placeholder="Event Venue" />
-                    </div>
-                    <div className={styles.formRow}>
-                        <div className={styles.formField}>
-                            <label htmlFor="date">Date</label>
-                            <input type="date" name="date" id="date" placeholder="Event Date" />
-                        </div>
-                        <div className={styles.formField}>
-                            <label htmlFor="time">Time</label>
-                            <input type="time" name="time" id="time" placeholder="Event Time" />
-                        </div>
-                    </div>
-                    <div className={styles.formField}>
-                        <label htmlFor="time">Category</label>
-                        <input type="time" name="time" id="time" placeholder="Event Time" />
-                    </div>
-                    <div className={styles.formField}>
-                        <label htmlFor="time">Tags</label>
-                        <input type="time" name="time" id="time" placeholder="Event Time" />
-                    </div>
-                    <div className={styles.formField}>
-                        <label htmlFor="time">Visibility</label>
-                        <select name="visibility">
-                            <option value="public">Public - Visible to everyone</option>
-                            <option value="private">Private - Visible to only people that have the link</option>
-                        </select>
-                    </div>
-                    <div className={styles.actionButtons}>
-                        <button type="submit">Next</button>
-                    </div>
-                </form>
-            </div>
+                    <button type="submit">Next</button>
+                </div>
+            </form>
         </div>
     );
 }
