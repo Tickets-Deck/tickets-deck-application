2caf731f97cc6165e9707dac7c11c335c6200e91
Simlex <simlexafol@gmail.com>
2023-12-28 09:06:37 +0100

    Add event creation functionality

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,4 +1,5 @@
 import { EventRequest } from "@/app/models/IEvents";
+import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -8,49 +9,56 @@ export async function POST(req: NextRequest) {
     console.log("request", request);
 
     try {
-      const { email } = request;
+      const { eventId } = request;
 
-      // If email is not provided, return 400
-      if (!email) {
+      // If eventId is not provided, return 400
+      if (!eventId) {
         return {
           status: 400,
-          json: { error: "Email is required" },
+          json: { error: "Event Id is required" },
         };
       }
 
-      // If email is not valid, return 400
-      if (!email.includes("@")) {
-        return {
-          status: 400,
-          json: { error: "Email is not valid" },
-        };
-      }
-
-      // Check if email already exists
-      const subscriberExists = await prisma.newsLetterSubscribers.findFirst({
+      // Check if ticket already exists
+      const eventExists = await prisma.events.findFirst({
         where: {
-          email,
+          eventId,
         },
       });
 
       // If email already exists, return 400
-      if (subscriberExists) {
+      if (eventExists) {
         return {
           status: 400,
-          json: { error: "Email already exists" },
+          json: { error: "Event already exists" },
         };
       }
 
-      // If email is valid, save it to the database
-      const subscriber = await prisma.newsLetterSubscribers.create({
+      // If eventId is valid, save it to the database
+      const event = await prisma.events.create({
         data: {
-          email,
+          eventId: request.eventId,
+          publisherId: request.publisherId,
+          title: request.title,
+          description: request.description,
+          // location: null,
+          venue: request.venue,
+          date: request.date,
+          time: request.time,
+          category: request.category,
+          // tags: request.tags,
+          visibility: request.visibility,
+          mainImageUrl: request.mainImageUrl,
+          currency: request.currency,
+          // tickets: request.tickets,
+          purchaseStartDate: request.purchaseStartDate,
+          purchaseEndDate: request.purchaseEndDate,
         },
       });
 
       return {
         status: 201,
-        json: subscriber,
+        json: event,
       };
     } catch (error) {
       console.error(error);
