334d9b9420e8d773058eddeb0ecf3b1116643ab7
Simlex <simlexafol@gmail.com>
2023-12-31 12:45:09 +0100

    Updated test base url

--- app/api/apiRoutes.ts
+++ app/api/apiRoutes.ts
@@ -8,6 +8,11 @@ export class ApiRoutes {
   static BASE_URL_DEV: string = "http://localhost:9000/";
 
   /**
+   * The test base url for the application
+   */
+  static BASE_URL_TEST: string = "https://ticketsdeck.netlify.app";
+
+  /**
    * The route to CreateNewsletterSubscriber endpoint
    */
   static CreateNewsletterSubscriber: string = "api/newsletter-subscriber";
