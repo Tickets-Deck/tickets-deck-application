ac1791c01ce96168e0f1668197e5f67b923d879f
Simlex <simlexafol@gmail.com>
2023-12-31 10:46:58 +0100

    Fix strokeWidth attribute in FullLoader component

--- app/components/Loader/FullLoader.tsx
+++ app/components/Loader/FullLoader.tsx
@@ -11,7 +11,7 @@ const FullPageLoader: FunctionComponent<FullPageLoaderProps> = ({backgroundColor
         <div className={LoaderStyle.loader} style={{backgroundColor:`${backgroundColor}`}}>
                 <div className={LoaderStyle.loadingSpinner}>
                     <svg className={LoaderStyle.loadingSpinner__circleSvg} viewBox="25 25 50 50">
-                        <circle className={LoaderStyle.loadingSpinner__circleStroke} cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+                        <circle className={LoaderStyle.loadingSpinner__circleStroke} cx="50" cy="50" r="20" fill="none" strokeWidth="2" stroke-miterlimit="10" />
                     </svg>
                 </div>
             </div>
