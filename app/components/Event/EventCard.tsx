32025a857f9d87dad463a7387d7132ecfa58dfa4
Simlex <simlexafol@gmail.com>
2023-12-31 10:46:05 +0100

    Refactor EventCard component and update shareEvent function

--- app/components/Event/EventCard.tsx
+++ app/components/Event/EventCard.tsx
@@ -6,27 +6,27 @@ import images from "../../../public/images";
 import useResponsive from "../../hooks/useResponsiveness";
 import styles from "../../styles/EventCard.module.scss";
 import moment from "moment";
-import { Event } from "../models/IEvent";
 import { ToastContext } from "../../extensions/toast";
+import { EventResponse } from "@/app/models/IEvents";
 
 interface EventCardProps {
-    event: Event
+    event: EventResponse
     mobileAndActionButtonDismiss?: boolean
-    consoleDisplay?: boolean 
+    consoleDisplay?: boolean
 }
 
 const EventCard: FunctionComponent<EventCardProps> = (
     { event, mobileAndActionButtonDismiss, consoleDisplay }): ReactElement => {
 
-    
+
     const windowRes = useResponsive();
     const onMobile = windowRes.width && windowRes.width < 768;
     const toasthandler = useContext(ToastContext);
 
-    function shareEvent() {
-        const eventURL = window.location.href;
+    function shareEvent(eventUrl: string) {
+        // const eventURL = window.location.href;
         try {
-            navigator.clipboard.writeText(eventURL);
+            navigator.clipboard.writeText(eventUrl);
             toasthandler?.logSuccess('Event link copied.', `The link to ${event.title} has been copied.`)
         } catch (error) {
             console.error("Copying to clipboard failed:", error);
@@ -55,7 +55,7 @@ const EventCard: FunctionComponent<EventCardProps> = (
             <span className={styles.event__tag}>Latest</span>
             <div className={styles.event__image}>
                 <Link href={consoleDisplay ? `/app/event/${event.id}` : `/event/${event.id}`}>
-                    <Image src={images.event_flyer} alt='Event flyer' />
+                    <Image src={event.mainImageUrl} alt='Event flyer' fill />
                 </Link>
             </div>
             <span className={styles.hLine}>
@@ -65,13 +65,14 @@ const EventCard: FunctionComponent<EventCardProps> = (
                 <div className={styles.eventInfo__lhs}>
                     <h3 className={styles.title}>{event.title}</h3>
                     <div className={styles.dateTime}>
-                        <span className={styles.dateTime__date}>{moment(event.dateCreated).format('MMM DD')}</span>
+                        <span className={styles.dateTime__date}>{moment(event.date).format('MMM DD')}</span>
                         <span className={styles.dateTime__dot}></span>
-                        <span className={styles.dateTime__time}>{moment(event.dateCreated).format('hh:mmA')}</span>
+                        <span className={styles.dateTime__time}>{event.time}</span>
                     </div>
                     <div className={styles.location}>
                         <LocationPinIcon />
-                        <p>{event.location.blockNumber + ' ' + event.location.city + ', ' + event.location.state + ', ' + event.location.country}</p>
+                        <p>{event.venue}</p>
+                        {/* <p>{event.location.blockNumber + ' ' + event.location.city + ', ' + event.location.state + ', ' + event.location.country}</p> */}
                     </div>
                 </div>
                 {mobileAndActionButtonDismiss ?
@@ -79,7 +80,18 @@ const EventCard: FunctionComponent<EventCardProps> = (
                     <div className={styles.eventInfo__rhs}>
                         <div className={styles.actions}>
                             <button className={styles.actions__like}><LikeIcon /></button>
-                            <button className={styles.actions__share} onClick={() => typeof (onMobile) == "boolean" && onMobile ? shareEventMobile() : shareEvent()}><ShareIcon /></button>
+                            <button
+                                className={styles.actions__share}
+                                onClick={() => {
+                                    shareEventMobile();
+                                    // if (typeof (onMobile) == "boolean" && onMobile) {
+                                    //     shareEventMobile()
+                                    // } else if (typeof (onMobile) == "boolean" && !onMobile) {
+                                    //     shareEvent(`${window.location.origin}/event/${event.id}`)
+                                    // }
+                                }}>
+                                <ShareIcon />
+                            </button>
                         </div>
                         <p className={styles.restriction}>Everyone</p>
                     </div>}
