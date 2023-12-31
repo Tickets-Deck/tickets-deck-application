de2d7563ef9df25d82bbfc12665588dd50ff68dd
Simlex <simlexafol@gmail.com>
2023-12-31 16:55:46 +0100

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -6,13 +6,23 @@ export function middleware(request: NextRequest) {
   const devUserIsAuthenticated = request.cookies.get("next-auth.session-token");
   const userIsAuthenticated = request.cookies.get("__Secure-next-auth.session-token");
 
-    // If the user is not authenticated, redirect to /auth/signin
-    if ((!devUserIsAuthenticated || !userIsAuthenticated) && request.nextUrl.pathname.startsWith("/app")) {
-        return NextResponse.redirect(new URL("/auth/signin", request.url));
-    } else {
+    if(request.nextUrl.pathname.startsWith("/app")) {
+        if(!devUserIsAuthenticated || !userIsAuthenticated) {
+            return NextResponse.redirect(new URL("/auth/signin", request.url));
+        }
         // Otherwise, let the request continue
         return NextResponse.next();
+    } else {
+        return NextResponse.next();
     }
+
+    // If the user is not authenticated, redirect to /auth/signin
+    // if ((!devUserIsAuthenticated || !userIsAuthenticated) && request.nextUrl.pathname.startsWith("/app")) {
+    //     return NextResponse.redirect(new URL("/auth/signin", request.url));
+    // } else {
+    //     // Otherwise, let the request continue
+    //     return NextResponse.next();
+    // }
 
 //   if (request.nextUrl.pathname.startsWith("/app")) {
 //     // This logic is only applied to pages that start with /app
