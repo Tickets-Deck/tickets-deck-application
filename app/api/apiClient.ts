187207aab7852e75faa41e8cb5cca5c58be426de
Simlex <simlexafol@gmail.com>
2023-12-31 10:40:12 +0100

    Add event-related API functions

--- app/api/apiClient.ts
+++ app/api/apiClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ApiRoutes } from "./apiRoutes";
 import { UserCredentialsRequest } from "../models/IUser";
+import { EventRequest } from "../models/IEvents";
 
 export const API = axios.create({
   baseURL: ApiRoutes.BASE_URL_DEV,
@@ -13,3 +14,37 @@ export function useCreateNewsletterSubscriber() {
 
   return createNewsletterSubscriber;
 }
+
+export function useCreateEvent() {
+  async function createEvent(event: EventRequest) {
+    return API.post(ApiRoutes.Events, event);
+  }
+
+  return createEvent;
+}
+
+export function useFetchEvents() {
+  async function fetchEvents() {
+    return API.get(ApiRoutes.Events);
+  }
+
+  return fetchEvents;
+}
+
+export function useFetchEventById() {
+  async function fetchEvent(id: string) {
+    return API.get(`${ApiRoutes.Events}?id=${id}`);
+  }
+
+  return fetchEvent;
+}
+
+export function useFetchEventsByEventId() {
+  async function fetchEventsByEventId(eventId: string) {
+    return API.get(`${ApiRoutes.Events}?eventId=${eventId}`);
+  }
+
+  return fetchEventsByEventId;
+}
+
+// export function useFetchEventsByPublisherId() {}
