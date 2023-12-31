c656c966fc1b9c2fb3b5f4487b45e407aa80c025
Simlex <simlexafol@gmail.com>
2023-12-31 10:48:04 +0100

    Cloudinary functions

---
+++ app/services/CloudinaryDelete.ts
@@ -0,0 +1,50 @@
+import { SetStateAction } from "react";
+import cloudinary from "cloudinary";
+
+export function deleteImageFromCloudinary(
+  imageId: string,
+  setIsDeletingingImage: (value: SetStateAction<boolean>) => void
+) {
+  const cloudName = "dvxqk1487";
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/destroy`;
+        // const url = `https://api.cloudinary.com/v1_1/${process.env.CLOUDNAME}/upload`
+
+  if (imageId) {
+    const formData = new FormData();
+    formData.append("resource_type", imageId); // image id is your public ID
+
+    // Start loader
+    setIsDeletingingImage(true);
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        // console.log("response gotten: ", response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data gotten after deletion: ", data);
+      })
+      .catch((error) => {
+        // console.log("error: ", error);
+      })
+      .finally(() => {
+        // Stop loader
+        setIsDeletingingImage(false);
+      });
+  }
+}
+
+export async function deleteImageFromCloudinaryv2(
+  imagePublicId: string,
+  setIsDeletingingImage: (value: SetStateAction<boolean>) => void
+) {
+    // Show loader
+    setIsDeletingingImage(true);
+
+    // Delete image from cloudinary
+    await cloudinary.v2.uploader.destroy(imagePublicId);
+}
---
+++ app/services/CloudinaryUpload.ts
@@ -0,0 +1,45 @@
+import { SetStateAction } from "react";
+
+export async function uploadImageToCloudinary(
+  imageFile: File | null,
+  setIsUploadingImage: (value: SetStateAction<boolean>) => void,
+  cloudinaryImageUrl: string | undefined,
+  setCloudinaryImageUrl: (value: SetStateAction<string | undefined>) => void
+) {
+  const cloudName = "dvxqk1487";
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+  // const url = `https://api.cloudinary.com/v1_1/${process.env.CLOUDNAME}/upload`
+
+  if (imageFile && !cloudinaryImageUrl) {
+    const formData = new FormData();
+    formData.append("file", imageFile);
+    formData.append("upload_preset", "event_images");
+    // formData.append("return_delete_token", "true"); // Not applicable for unsigned uploads
+
+    // Start loader
+    setIsUploadingImage(true);
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        // console.log("response gotten: ", response);
+        return response.json();
+      })
+      .then((data) => {
+        // console.log("data gotten: ", data);
+        setCloudinaryImageUrl(data.secure_url);
+
+        // Stop loader
+        setIsUploadingImage(false);
+      })
+      .catch((error) => {
+        // Stop loader
+        setIsUploadingImage(false);
+      });
+  }
+
+  return cloudinaryImageUrl;
+}
