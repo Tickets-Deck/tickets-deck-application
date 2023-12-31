9380ec1b08fce4a596a9f65615f72574fde54255
Simlex <simlexafol@gmail.com>
2023-12-31 10:38:54 +0100

    Refactor authentication options and callbacks

--- auth.ts
+++ auth.ts
@@ -66,25 +66,68 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
-          redirect_uri: "http://localhost:9000/api/auth/callback/google", // Make sure this matches the URI in the Google Developer Console
+          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`, // Make sure this matches the URI in the Google Developer Console
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
         },
-      }
+      },
     }),
   ],
   secret: process.env.SECRET,
   callbacks: {
+    async signIn({ account, profile }) {
+      console.log("Sign In Callback", { account, profile });
+
+      if (account?.provider === "google") {
+        // Check if user exists in database checking each user's email if it matches the email provided
+        const user = await prisma.users.findUnique({
+          where: {
+            email: profile?.email,
+          },
+        });
+
+        // If user exists, return true to allow sign in
+        if (user) {
+          return true; // Return true to allow sign in
+        }
+
+        // If user does not exist, create user
+        await prisma.users.create({
+          data: {
+            email: profile?.email as string,
+            firstName: profile?.name?.split(" ")[0] as string,
+            lastName: profile?.name?.split(" ")[1] as string,
+            password: "google-signup-no-password",
+            image: profile?.picture as string,
+          },
+        });
+
+        return true; // Return true to allow sign in
+      }
+      return true; // Return true to allow sign in
+    },
+    async redirect({ url, baseUrl }) {
+      return process.env.NEXTAUTH_URL as string;
+    },
     // Create and manage JWTs here
-    jwt: ({ token, user, trigger, session }) => {
+    jwt: async ({ token, user, trigger, session }) => {
       console.log("JWT Callback", { token, user });
 
-      if (trigger === "update") {
-        return { ...token, ...session.user };
-      }
+      // Check prisma for user with email gotten in token
+      const exisitingUser = await prisma.users.findUnique({
+        where: {
+          email: token.email as string,
+        },
+      });
 
-      if (user) {
+      // If user exists, return user id in JWT so it will be accessible in the session
+      if (exisitingUser) {
+        return {
+          ...token,
+          id: exisitingUser.id,
+        };
+      } else if (user) {
         const u = user as unknown as any;
 
         // Return user id and randomKey in JWT so it will be accessible in the session
@@ -94,6 +137,11 @@ export const authOptions: NextAuthOptions = {
           id: u.id,
         };
       }
+
+      if (trigger === "update") {
+        return { ...token, ...session.user };
+      }
+
       return token;
     },
     // Create and manage sessions here
@@ -110,4 +158,4 @@ export const authOptions: NextAuthOptions = {
       };
     },
   },
-}
+};
