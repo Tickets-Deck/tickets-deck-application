4f777c925f4d781a32479a3e8cdcd59fcc7f3106
Simlex <simlexafol@gmail.com>
2023-12-31 10:44:53 +0100

    Added page to create event

--- app/app/event/create/page.tsx
+++ app/app/event/create/page.tsx
@@ -1,21 +1,21 @@
 "use client"
-import React, { FunctionComponent, ReactElement, useCallback, useRef, useState, FormEvent, ChangeEvent, Dispatch, SetStateAction, useEffect } from "react";
+import React, { FunctionComponent, ReactElement, useState, FormEvent, useEffect } from "react";
 import styles from '../../../styles/CreateEvent.module.scss';
 import CreateEventProgressBar from "../../../components/shared/CreateEventProgressBar";
-import { AddEventIcon, AddIcon, CalenderIcon, CloseIcon, PhotoIcon } from "@/app/components/SVGs/SVGicons";
-import { DatePicker, ITimeRange } from "@fluentui/react";
-import { categories } from "@/app/constants/eventCategories";
+import { CloseIcon } from "@/app/components/SVGs/SVGicons";
 import { EventRequest } from "@/app/models/IEvents";
-import useOuterClick from "@/app/hooks/useOuterClick";
 import { EventVisibility } from "@/app/enums/IEventVisibility";
 import BasicInformationForm from "@/app/components/Event/Create/BasicInformationForm";
 import { ValidationStatus } from "@/app/enums/BasicInfoFormValidationStatus";
-import Image from "next/image";
-import images from "@/public/images";
 import { EventCreationStage } from "@/app/enums/EventCreationStage";
 import ImageUploadSection from "@/app/components/Event/Create/ImageUploadSection";
-import { Tickets } from "@/app/models/ITicket";
 import TicketDetailsSection from "@/app/components/Event/Create/TicketDetailsSection";
+import ConfirmationSection from "../../../components/Event/Create/ConfirmationSection";
+import { useCreateEvent } from "@/app/api/apiClient";
+import ComponentLoader from "@/app/components/Loader/ComponentLoader";
+import { uploadImageToCloudinary } from "@/app/services/CloudinaryUpload";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface CreateEventProps {
 
@@ -23,9 +23,19 @@ interface CreateEventProps {
 
 const CreateEvent: FunctionComponent<CreateEventProps> = (): ReactElement => {
 
+    const createEvent = useCreateEvent();
+    const { data: session } = useSession();
+    const {push} = useRouter();
+
     const [eventCreationStage, setEventCreationStage] = useState<EventCreationStage>(EventCreationStage.BasicInfo);
     const [eventRequest, setEventRequest] = useState<EventRequest>();
     const [validationStage, setValidationStage] = useState<{ status: ValidationStatus }>();
+
+    const [mainImageFile, setMainImageFile] = useState<File>();
+    const [cloudinaryImageUrl, setCloudinaryImageUrl] = useState<string>();
+    const [isUploadingMainImage, setIsUploadingMainImage] = useState(false);
+
+    const [isCreatingEvent, setIsCreatingEvent] = useState(false);
 
     function proceedToImageUpload() {
 
@@ -51,82 +61,130 @@ const CreateEvent: FunctionComponent<CreateEventProps> = (): ReactElement => {
                 proceedToImageUpload();
                 break;
             case EventCreationStage.ImageUpload:
+                setEventRequest({ ...eventRequest as EventRequest, tickets: [] });
                 setEventCreationStage(EventCreationStage.TicketDetails);
                 break;
             case EventCreationStage.TicketDetails:
                 setEventCreationStage(EventCreationStage.Confirmation);
                 break;
             case EventCreationStage.Confirmation:
-                setEventCreationStage(EventCreationStage.BasicInfo);
+                handleEventCreation();
                 break;
         }
     };
 
-    // useEffect(() => {
-    //     console.log(eventRequest);
-    // }, [eventCreationStage]); 
+    console.log("User ID: ", session?.user.id);
+
+    async function handleEventCreation() {
+
+        // Upload image to cloudinary
+        const imageUrlResponse = await uploadImageToCloudinary(mainImageFile as File, setIsUploadingMainImage, cloudinaryImageUrl, setCloudinaryImageUrl)
+
+        console.log('imageUrlResponse gotten1: ', imageUrlResponse);
+        if (!imageUrlResponse) {
+            console.log("Error uploading image to cloudinary");
+            return;
+        }
+
+        console.log('imageUrlResponse gotten2: ', imageUrlResponse);
+
+        // Start loader
+        setIsCreatingEvent(true);
+
+        // Create the event
+        await createEvent({ ...eventRequest as EventRequest, mainImageUrl: imageUrlResponse as string })
+            .then((response) => {
+                // log response
+                console.log(response);
+                // Clear the event request
+                setEventRequest(undefined);
+                // Redirect to the event page
+                push(`/app/event/${response.data.id}`);
+            })
+            .catch((error) => {
+                // log error
+                console.log(error);
+            })
+            .finally(() => {
+                // Stop loader
+                setIsCreatingEvent(false);
+            });
+};
+
+useEffect(() => {
+    setEventRequest({ ...eventRequest as EventRequest, publisherId: session?.user.id as string });
+}, [session]);
 
 
-    return (
-        <div className={styles.main}>
-            <div className={styles.topArea}>
-                <h3>Create Event</h3>
-                {/* <Link href="/app/event/create">
+return (
+    <div className={styles.main}>
+        <div className={styles.topArea}>
+            <h3>Create Event</h3>
+            {/* <Link href="/app/event/create">
                     <button>New Event</button>
                 </Link> */}
-            </div>
-
-            <CreateEventProgressBar
-                eventCreationStage={eventCreationStage}
-                setEventCreationStage={setEventCreationStage}
-                eventRequest={eventRequest}
-            />
-
-            <form onSubmit={moveToNextStage}>
-                {eventCreationStage === EventCreationStage.BasicInfo &&
-                    <BasicInformationForm
-                        eventRequest={eventRequest}
-                        setEventRequest={setEventRequest}
-                        validationStage={validationStage}
-                        setValidationStage={setValidationStage}
-                        setEventCreationStage={setEventCreationStage}
-                    />}
-
-                {eventCreationStage === EventCreationStage.ImageUpload &&
-                    <ImageUploadSection
-                        eventRequest={eventRequest}
-                        setEventRequest={setEventRequest}
-                    />}
-
-                {eventCreationStage === EventCreationStage.TicketDetails &&
-                    <TicketDetailsSection
-                        eventRequest={eventRequest}
-                        setEventRequest={setEventRequest}
-                    />
-                }
-
-                {eventCreationStage === EventCreationStage.Confirmation &&
-                    <div>Confirmation</div>
-                }
-
-                <div className={styles.actionButtons}>
-                    <div className={styles.tagSection}>
-                        {
-                            eventRequest?.tags?.map((tag, index) => {
-                                return (
-                                    <span className={styles.tag} key={index}> 
-                                        {tag}
-                                        <span><CloseIcon /></span>
-                                    </span>
-                                )
-                            })
-                        }
-                    </div>
-                    <button type="submit">Next</button>
-                </div>
-            </form>
         </div>
-    );
+
+        <CreateEventProgressBar
+            eventCreationStage={eventCreationStage}
+            setEventCreationStage={setEventCreationStage}
+            eventRequest={eventRequest}
+        />
+
+        <form onSubmit={moveToNextStage}>
+            {eventCreationStage === EventCreationStage.BasicInfo &&
+                <BasicInformationForm
+                    eventRequest={eventRequest}
+                    setEventRequest={setEventRequest}
+                    validationStage={validationStage}
+                    setValidationStage={setValidationStage}
+                    setEventCreationStage={setEventCreationStage}
+                />}
+
+            {eventCreationStage === EventCreationStage.ImageUpload &&
+                <ImageUploadSection
+                    eventRequest={eventRequest}
+                    setEventRequest={setEventRequest}
+                    mainImageFile={mainImageFile}
+                    setMainImageFile={setMainImageFile}
+                />}
+
+            {eventCreationStage === EventCreationStage.TicketDetails &&
+                <TicketDetailsSection
+                    eventRequest={eventRequest}
+                    setEventRequest={setEventRequest}
+                />
+            }
+
+            {eventCreationStage === EventCreationStage.Confirmation &&
+                <ConfirmationSection
+                    eventRequest={eventRequest}
+                    setEventRequest={setEventRequest}
+                />
+            }
+
+            <div className={styles.actionButtons}>
+                <div className={styles.tagSection}>
+                    {
+                        eventRequest?.tags?.map((tag, index) => {
+                            return (
+                                <span className={styles.tag} key={index}>
+                                    {tag}
+                                    <span><CloseIcon /></span>
+                                </span>
+                            )
+                        })
+                    }
+                </div>
+                <button type="submit" disabled={isCreatingEvent || isUploadingMainImage}>
+                    {/* { 'Next'} */}
+                    {eventCreationStage === EventCreationStage.Confirmation ? isUploadingMainImage ? 'Uploading images' : isCreatingEvent ? 'Creating event' : 'Create Event' : 'Next'}
+                    {isCreatingEvent && <ComponentLoader isSmallLoader customBackground="#fff" customLoaderColor="#111111" />}
+                </button>
+            </div>
+        </form>
+    </div>
+);
 }
 
 export default CreateEvent;
