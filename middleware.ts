66b717761bdb0bc1529bf9c6302e6c21a416058c
Simlex <simlexafol@gmail.com>
2024-01-03 08:47:38 +0100

    Refactor middleware and update withAuth configuration

--- middleware.ts
+++ middleware.ts
@@ -17,13 +17,13 @@ import { withAuth } from "next-auth/middleware";
 //         return NextResponse.next();
 //     }
 
-    // If the user is not authenticated, redirect to /auth/signin
-    // if ((!devUserIsAuthenticated || !userIsAuthenticated) && request.nextUrl.pathname.startsWith("/app")) {
-    //     return NextResponse.redirect(new URL("/auth/signin", request.url));
-    // } else {
-    //     // Otherwise, let the request continue
-    //     return NextResponse.next();
-    // }
+// If the user is not authenticated, redirect to /auth/signin
+// if ((!devUserIsAuthenticated || !userIsAuthenticated) && request.nextUrl.pathname.startsWith("/app")) {
+//     return NextResponse.redirect(new URL("/auth/signin", request.url));
+// } else {
+//     // Otherwise, let the request continue
+//     return NextResponse.next();
+// }
 
 //   if (request.nextUrl.pathname.startsWith("/app")) {
 //     // This logic is only applied to pages that start with /app
@@ -32,13 +32,24 @@ import { withAuth } from "next-auth/middleware";
 //     // Otherwise, let the request continue
 //     return NextResponse.next();
 //   }
-
 // }
 
 export default withAuth({
-    secret: process.env.SECRET,
-  });
-
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    authorized: ({ req, token }) => {
+      // verify token and return a boolean
+      //   const sessionToken = req.cookies.get("next-auth.session-token");
+      //   if (sessionToken) return true;
+      //   else return false;
+      if (token === null) return false;
+      else return true;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
 
 // See "Matching Paths" below to learn more
 export const config = {
