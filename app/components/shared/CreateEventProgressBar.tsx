12b9811ae8370de17d5b6759a3b241af147422f0
Simlex <simlexafol@gmail.com>
2024-01-11 00:23:26 +0100

    Refactor CreateEventProgressBar component to disable tabs when disableAllTabs prop is true

--- app/components/shared/CreateEventProgressBar.tsx
+++ app/components/shared/CreateEventProgressBar.tsx
@@ -7,10 +7,11 @@ interface CreateEventProgressBarProps {
     eventCreationStage: EventCreationStage
     setEventCreationStage: Dispatch<SetStateAction<EventCreationStage>>
     eventRequest: EventRequest | undefined
+    disableAllTabs: boolean
 }
 
 const CreateEventProgressBar: FunctionComponent<CreateEventProgressBarProps> = (
-    { eventCreationStage, setEventCreationStage, eventRequest }): ReactElement => {
+    { eventCreationStage, setEventCreationStage, eventRequest, disableAllTabs }): ReactElement => {
 
     const imageUploadIsUndone = eventCreationStage === EventCreationStage.BasicInfo && !eventRequest?.mainImageUrl;
     const ticketDetailsIsUndone = eventCreationStage < EventCreationStage.TicketDetails && !eventRequest?.tickets;
@@ -30,23 +31,29 @@ const CreateEventProgressBar: FunctionComponent<CreateEventProgressBarProps> = (
         <div className={styles.progressBarContainer}>
             <span className={styles.indicator}></span>
             <div
+                style={disableAllTabs ? { pointerEvents: 'none' } : {}}
                 className={`${styles.stage} ${basicInfoIsCurrent ? styles.currentStage : ""} ${basicInfoIsDone ? styles.doneStage : ""} `}
                 onClick={() => setEventCreationStage(EventCreationStage.BasicInfo)}>
                 <span className={styles.stageNumber}>1</span>
                 <p className={styles.stageTitle}>Event Details</p>
             </div>
-            <div
+            <div 
+                style={disableAllTabs ? { pointerEvents: 'none' } : {}}
                 className={`${styles.stage} ${imageUploadIsCurrent ? styles.currentStage : ''} ${imageUploadIsDone ? styles.doneStage : ''} ${imageUploadIsUndone ? styles.unDoneStage : ''} `}
                 onClick={() => setEventCreationStage(EventCreationStage.ImageUpload)}>
                 <span className={styles.stageNumber}>2</span>
                 <p className={styles.stageTitle}>Image Upload</p>
             </div>
-            <div className={`${styles.stage} ${ticketDetailsIsCurrent ? styles.currentStage : ''} ${ticketDetailsIsDone ? styles.doneStage : ''} ${ticketDetailsIsUndone ? styles.unDoneStage : ''} `}
+            <div
+                style={disableAllTabs ? { pointerEvents: 'none' } : {}}
+                className={`${styles.stage} ${ticketDetailsIsCurrent ? styles.currentStage : ''} ${ticketDetailsIsDone ? styles.doneStage : ''} ${ticketDetailsIsUndone ? styles.unDoneStage : ''} `}
                 onClick={() => setEventCreationStage(EventCreationStage.TicketDetails)}>
                 <span className={styles.stageNumber}>3</span>
                 <p className={styles.stageTitle}>Ticket Details</p>
             </div>
-            <div className={`${styles.stage} ${reviewAndPublishIsCurrent ? styles.currentStage : ''} ${reviewAndPublishIsDone ? styles.doneStage : ''} ${reviewAndPublishIsUndone ? styles.unDoneStage : ''} `}
+            <div
+                style={disableAllTabs ? { pointerEvents: 'none' } : {}}
+                className={`${styles.stage} ${reviewAndPublishIsCurrent ? styles.currentStage : ''} ${reviewAndPublishIsDone ? styles.doneStage : ''} ${reviewAndPublishIsUndone ? styles.unDoneStage : ''} `}
                 onClick={() => setEventCreationStage(EventCreationStage.Confirmation)}>
                 <span className={styles.stageNumber}>4</span>
                 <p className={styles.stageTitle}>Review & Publish</p>
