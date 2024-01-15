d5a577401d0b0c5929f4be1b59180873a34ac5dd
Simlex <simlexafol@gmail.com>
2024-01-15 13:30:00 +0100

    Add user search functionality and send email on account creation

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,9 +2,41 @@ import { UserCredentialsRequest } from "@/app/models/IUser";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { compileAccountCreationTemplate, sendMail } from "@/lib/mail";
 
 export async function GET(req: NextRequest) {
   if (req.method === "GET") {
+    // Get the search params from the request url
+    const searchParams = new URLSearchParams(req.url.split("?")[1]);
+
+    // Get the userId from the search params
+    const userId = searchParams.get("userId");
+
+    // If a userId is provided, find the user with that id
+    if (userId) {
+      const user = await prisma.users.findUnique({
+        where: {
+          id: userId,
+        },
+        include: {
+            events: true,
+        }
+      });
+
+      // If user is not found, return 404
+      if (!user) {
+        return NextResponse.json(
+          { error: "User with specified User ID not found" },
+          { status: 404 }
+        );
+      }
+
+      // If user is found, return it
+      if (user) {
+        return NextResponse.json(user, { status: 200 });
+      }
+    }
+
     // Fetch all users
     const users = await prisma.users.findMany();
 
@@ -46,6 +78,16 @@ export async function POST(req: NextRequest) {
         lastName: request.lastName,
         phone: request.phone ?? null,
       },
+    });
+
+    // Send email to the new customer
+    await sendMail({
+      to: request.email,
+      name: "Account Created",
+      subject: "Welcome to Tickets Deck",
+      body: compileAccountCreationTemplate(
+        `${request.firstName} ${request.lastName}`
+      ),
     });
 
     // Return the new user, and a message that the user was created
