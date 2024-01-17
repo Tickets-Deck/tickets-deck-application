fd47926467ec3ec75219496af39bca53ef90d3bb
Simlex <simlexafol@gmail.com>
2024-01-17 03:07:48 +0100

    Add UserCredentialsUpdateRequest and PUT route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,7 @@
-import { UserCredentialsRequest } from "@/app/models/IUser";
+import {
+  UserCredentialsRequest,
+  UserCredentialsUpdateRequest,
+} from "@/app/models/IUser";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
@@ -19,8 +22,8 @@ export async function GET(req: NextRequest) {
           id: userId,
         },
         include: {
-            events: true,
-        }
+          events: true,
+        },
       });
 
       // If user is not found, return 404
@@ -95,6 +98,77 @@ export async function POST(req: NextRequest) {
       { detail: "Successfully created a new user", user: newUser },
       { status: 201 }
     );
+  } else {
+    return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  if (req.method === "PUT") {
+    // Get the search params from the request url
+    const searchParams = new URLSearchParams(req.url.split("?")[1]);
+
+    // Get the userId from the search params
+    const userId = searchParams.get("userId");
+
+    // Get the body of the request
+    const request = (await req.json()) as UserCredentialsUpdateRequest;
+
+    // If userId is not provided, return 400
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User ID is required" },
+        { status: 400 }
+      );
+    }
+
+    //find the current user with the specified id
+    const user = await prisma.users.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    // If user is not found, return 404
+    if (!user) {
+      return NextResponse.json(
+        { error: "User with specified User ID not found" },
+        { status: 404 }
+      );
+    }
+
+    if (request.email && request.email.toLowerCase() !== user.email.toLowerCase()) {
+      // Check if the email is already taken
+      const emailExists = await prisma.users.findUnique({
+        where: {
+          email: request.email,
+        },
+      });
+
+      // If email is already taken, return 400
+      if (emailExists) {
+        return NextResponse.json(
+          { error: "Email is already taken" },
+          { status: 400 }
+        );
+      }
+    }
+
+    // If user is found, update the user
+    const updatedUser = await prisma.users.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        email: request.email?.toLowerCase() ?? user.email.toLowerCase(),
+        firstName: request.firstName ?? user.firstName, 
+        lastName: request.lastName ?? user.lastName,
+        phone: request.phone ?? user.phone,
+      },
+    });
+
+    // Return the updated user
+    return NextResponse.json(updatedUser, { status: 200 });
   } else {
     return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
   }
