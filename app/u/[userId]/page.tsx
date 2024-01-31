7b49a132aed58f2b254e5208a2f8b321e1b0a43d
Simlex <simlexafol@gmail.com>
2024-01-31 10:10:11 +0100

    Add session to UserInformationPage component

--- app/u/[userId]/page.tsx
+++ app/u/[userId]/page.tsx
@@ -1,13 +1,21 @@
 import { ReactElement, FunctionComponent } from "react";
 import UserInformationPage from "./UserInformationPage";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth";
 
 interface UserInformationProps {
     params: { userId: string }
 }
 
-const UserInformation: FunctionComponent<UserInformationProps> = ({ params }): ReactElement => {
+const UserInformation: FunctionComponent<UserInformationProps> = async ({ params }) => {
+
+    const session = await getServerSession(authOptions);
+
     return (
-        <UserInformationPage identifier={params.userId} />
+        <UserInformationPage
+            identifier={params.userId}
+            session={session}
+        />
     );
 }
 
