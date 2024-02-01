c205951fa27f7ea998f0ee0e392ff7dccaf8e5dd
Simlex <simlexafol@gmail.com>
2024-02-01 19:00:16 +0100

    Add UserPersonalInfo, UserSocials, UserStats, and UserHighlights components

--- app/u/[userId]/UserInformationPage.tsx
+++ app/u/[userId]/UserInformationPage.tsx
@@ -10,6 +10,10 @@ import ComponentLoader from "@/app/components/Loader/ComponentLoader";
 import UserCoverContainer from "@/app/components/ProfilePage/UserCoverContainer";
 import { FacebookIcon, InstagramIcon, TwitterIcon } from "@/app/components/SVGs/SVGicons";
 import { Session } from "next-auth";
+import UserPersonalInfo from "@/app/components/UserInformationPage/UserPersonalInfo";
+import UserSocials from "@/app/components/UserInformationPage/UserSocials";
+import UserStats from "@/app/components/UserInformationPage/UserStats";
+import UserHighlights from "@/app/components/UserInformationPage/UserHighlights";
 
 interface UserInformationPageProps {
     identifier: string
@@ -71,90 +75,23 @@ const UserInformationPage: FunctionComponent<UserInformationPageProps> = ({ iden
                         forUser
                     />
                     <div className={styles.profilePage__body}>
-                        <div className={styles.userPersonalInfo}>
-                            <div className={styles.userPersonalInfo__avatar}>
-                                <UserAvatarContainer
-                                    userInformation={userInformation}
-                                    userAvatarSize={140}
-                                />
-                            </div>
-                            <div className={styles.userPersonalInfo__info}>
-                                <h3 className={styles.userPersonalInfo__info__name}>
-                                    {userInformation?.firstName} {userInformation?.lastName}
-                                </h3>
-                                {
-                                    userInformation?.username &&
-                                    <p className={styles.userPersonalInfo__info__username}>
-                                        @{userInformation?.username}
-                                    </p>
-                                }
-                            </div>
-                        </div>
-                        <div className={styles.userSocials}>
-                            {
-                                userInformation.facebookUrl &&
-                                <Link href="#">
-                                    <span><FacebookIcon /></span>
-                                </Link>
-                            }
-                            {
-                                userInformation.instagramUrl &&
-                                <Link href={userInformation.instagramUrl}>
-                                    <span><InstagramIcon /></span>
-                                </Link>
-                            }
-                            {
-                                userInformation.twitterUrl &&
-                                <Link href="#">
-                                    <span><TwitterIcon /></span>
-                                </Link>
-                            }
-                        </div>
-                        <div className={styles.userStats}>
-                            <div className={styles.stat}>
-                                <p className={styles.stat__number}>
-                                    {userInformation.eventsCount}
-                                </p>
-                                <p className={styles.stat__label}>
-                                    Events
-                                </p>
-                            </div>
-                            {/* <div className={styles.stat}>
-                                <p className={styles.stat__number}>
-                                    {userInformation.ticketsPurchasedCount}
-                                </p>
-                                <p className={styles.stat__label}>
-                                    Tickets
-                                </p>
-                            </div> */}
-                            <div className={styles.stat}>
-                                <p className={styles.stat__number}>
-                                    {userInformation.followersCount}
-                                </p>
-                                <p className={styles.stat__label}>
-                                    Followers
-                                </p>
-                            </div>
-                            <div className={styles.stat}>
-                                <p className={styles.stat__number}>
-                                    {userInformation.followingCount}
-                                </p>
-                                <p className={styles.stat__label}>
-                                    Following
-                                </p>
-                            </div>
-                        </div>
+                        <UserPersonalInfo
+                            userInformation={userInformation}
+                        />
+                        <UserSocials
+                            userInformation={userInformation}
+                        />
+                        <UserStats
+                            userInformation={userInformation}
+                        />
                         {isForUser &&
                             <Link href="/app/profile" className={styles.updateProfileBtn}>
                                 Update Profile
                             </Link>
                         }
-                        <div className={styles.userHighlights}>
-                            <h3>Recent Events</h3>
-                            <div className={styles.noHighlightsContainer}>
-                                <p>No events yet</p>
-                            </div>
-                        </div>
+                        <UserHighlights
+                            userInformation={userInformation}
+                        />
                     </div>
                 </>
             }
