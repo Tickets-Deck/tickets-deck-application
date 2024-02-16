1593f16b4709d223bc82b0271ef0bc290b5ef8f2
Simlex <simlexafol@gmail.com>
2024-02-17 00:22:27 +0100

    Add user session and Redux selector to VerifyPaymentPage

--- app/verify-payment/VerifyPaymentPage.tsx
+++ app/verify-payment/VerifyPaymentPage.tsx
@@ -4,6 +4,9 @@ import styles from "../styles/VerifyPaymentPage.module.scss"
 import ComponentLoader from "../components/Loader/ComponentLoader";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useVerifyPaystackPayment } from "../api/apiClient";
+import { useSession } from "next-auth/react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 interface VerifyPaymentPageProps {
 
@@ -19,22 +22,56 @@ const VerifyPaymentPage: FunctionComponent<VerifyPaymentPageProps> = (): ReactEl
 
     const searchParams = useSearchParams();
     const router = useRouter();
+    const userInfo = useSelector((state: RootState) => state.userCredentials.userInfo);
     const verifyPaystackPayment = useVerifyPaystackPayment();
 
-    const [isVerifyingPayment, setIsVerifyingPayment] = useState(true);
+    const [isVerifyingPayment, setIsVerifyingPayment] = useState(false);
     const [paymentStatus, setPaymentStatus] = useState(PaymentStatus.Verifying);
 
     const trxref = searchParams.get('trxref');
 
-    async function handlePaymentVerification() {
+    /**
+     * Function to handle the verification of the payment
+     * @param trxref is the transaction reference returned from the paystack payment gateway
+     */
+    // async function handlePaymentVerification(trxref: string) {
+    //     try {
+    //         setIsVerifyingPayment(true);
+    //         const response = await verifyPaystackPayment(trxref);
+    //         // console.log(response);
+    //         setPaymentStatus(PaymentStatus.Success);
+    //         if (response) {
+    //             if (userInfo) {
+    //                 router.push('/app');
+    //                 return;
+    //             }
+    //             router.push('/');
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //         setPaymentStatus(PaymentStatus.Failed);
+    //     }
+    // }
+
+    async function handlePaymentVerification(trxref: string) {
+        // Start loader
+        setIsVerifyingPayment(true);
 
         await verifyPaystackPayment(trxref as string)
             .then((response) => {
-                console.log(response);
+                // console.log(response);
                 // Set payment status state
                 setPaymentStatus(PaymentStatus.Success);
+
+                /// TODO: Show a success message to the user and action button to redirect to user dashboard or home page
+                
                 // Route to user dashboard
-                router.push('app');
+                if (userInfo) {
+                    router.push('/app');
+                    return;
+                } else {
+                    router.push('/');
+                }
             })
             .catch((error) => {
                 // Set payment status state
@@ -43,16 +80,18 @@ const VerifyPaymentPage: FunctionComponent<VerifyPaymentPageProps> = (): ReactEl
             })
     };
 
+    // Write a useEffect to handle the payment verification once the component mounts, and making sure it only runs once
+
     useEffect(() => {
-        if (trxref) {
-            handlePaymentVerification();
+        if (trxref && !isVerifyingPayment) {
+            handlePaymentVerification(trxref);
         }
-    }, [router])
+    }, [trxref]);
 
     return (
         <main className={styles.verifyPaymentPage}>
             {
-                paymentStatus == PaymentStatus.Verifying &&
+                (paymentStatus == PaymentStatus.Verifying || paymentStatus == PaymentStatus.Success) &&
                 <div className={styles.loaderAreaContainer}>
                     <div className={styles.loaderArea}>
                         <ComponentLoader customLoaderColor="#fff" />
