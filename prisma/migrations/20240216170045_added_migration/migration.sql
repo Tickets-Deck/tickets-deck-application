33d10095fbfdba496a9bf130431a5b5260a7e4e2
Simlex <simlexafol@gmail.com>
2024-02-17 00:22:33 +0100

    Update decimal column precision and scale

---
+++ prisma/migrations/20240216170045_added_migration/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `price` on the `orderedtickets` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,2)` to `Decimal(65,30)`.
+  - You are about to alter the column `amount` on the `payment` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,2)` to `Decimal(65,30)`.
+  - You are about to alter the column `amountPaid` on the `payment` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,2)` to `Decimal(65,30)`.
+  - You are about to alter the column `totalPrice` on the `ticketorders` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,2)` to `Decimal(65,30)`.
+  - You are about to alter the column `price` on the `tickets` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,2)` to `Decimal(65,30)`.
+  - You are about to alter the column `totalRevenue` on the `users` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,2)` to `Decimal(65,30)`.
+
+*/
+-- AlterTable
+ALTER TABLE `Orderedtickets` MODIFY `price` DECIMAL(65, 30) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Payment` MODIFY `amount` DECIMAL(65, 30) NOT NULL,
+    MODIFY `amountPaid` DECIMAL(65, 30) NULL;
+
+-- AlterTable
+ALTER TABLE `Ticketorders` MODIFY `totalPrice` DECIMAL(65, 30) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Tickets` MODIFY `price` DECIMAL(65, 30) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Users` MODIFY `totalRevenue` DECIMAL(65, 30) NOT NULL DEFAULT 0;
