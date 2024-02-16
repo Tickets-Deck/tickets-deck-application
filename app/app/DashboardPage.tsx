405b64aa5acb4d1c2dcc32f2b9948bee08826437
Simlex <simlexafol@gmail.com>
2024-02-17 00:20:18 +0100

    Add fetchDashboardInfo function and display dashboard information

--- app/app/DashboardPage.tsx
+++ app/app/DashboardPage.tsx
@@ -5,6 +5,9 @@ import { EventIcon } from "../components/SVGs/SVGicons";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { useFetchDashboardInfo } from "../api/apiClient";
+import { DashboardInfoResponse } from "../models/IDashboardInfoResponse";
+import { catchError } from "../constants/catchError";
 
 interface DashboardPageProps {
 }
@@ -34,17 +37,32 @@ interface DashboardPageProps {
 
 const DashboardPage: FunctionComponent<DashboardPageProps> = (): ReactElement => {
 
+    const fetchDashboardInfo = useFetchDashboardInfo();
     const { data: session } = useSession();
     const user = session?.user;
     const { push, prefetch } = useRouter();
 
+    const [dashboardInfo, setDashboardInfo] = useState<DashboardInfoResponse>();
     const [isLoading, setIsLoading] = useState(false);
 
-    // useEffect(() => {
-    //     if (!session) {
-    //         push('/auth/signin');
-    //     }
-    // }, [session])
+    async function handleFetchDashboardInfo() {
+        // Start loader
+        setIsLoading(true);
+        try {
+            const response = await fetchDashboardInfo(user?.id as string);
+            setDashboardInfo(response.data);
+        } catch (error) {
+            catchError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (user) {
+            handleFetchDashboardInfo();
+        }
+    }, [user]);
 
     return (
         <div className={styles.dashboard}>
@@ -55,36 +73,39 @@ const DashboardPage: FunctionComponent<DashboardPageProps> = (): ReactElement =>
                 </Link>
             </div>
 
-            <div className={styles.kpiSection}>
-                <div className={styles.kpi}>
-                    <span><EventIcon /></span>
-                    <div className={styles.content}>
-                        <h4>0</h4>
-                        <p>Total Events</p>
+            {
+                dashboardInfo &&
+                <div className={styles.kpiSection}>
+                    <div className={styles.kpi}>
+                        <span><EventIcon /></span>
+                        <div className={styles.content}>
+                            <h4>{dashboardInfo.totalEvents}</h4>
+                            <p>Total Events</p>
+                        </div>
+                    </div>
+                    <div className={styles.kpi}>
+                        <span><EventIcon /></span>
+                        <div className={styles.content}>
+                            <h4>{dashboardInfo.ticketsBought}</h4>
+                            <p>Tickets Bought</p>
+                        </div>
+                    </div>
+                    <div className={styles.kpi}>
+                        <span><EventIcon /></span>
+                        <div className={styles.content}>
+                            <h4>{dashboardInfo.ticketsSold}</h4>
+                            <p>Tickets Sold</p>
+                        </div>
+                    </div>
+                    <div className={styles.kpi}>
+                        <span><EventIcon /></span>
+                        <div className={styles.content}>
+                            <h4>&#8358;{dashboardInfo.totalRevenue.toLocaleString()}</h4>
+                            <p>Total Revenue</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.kpi}>
-                    <span><EventIcon /></span>
-                    <div className={styles.content}>
-                        <h4>0</h4>
-                        <p>Tickets Bought</p>
-                    </div>
-                </div>
-                <div className={styles.kpi}>
-                    <span><EventIcon /></span>
-                    <div className={styles.content}>
-                        <h4>0</h4>
-                        <p>Tickets Sold</p>
-                    </div>
-                </div>
-                <div className={styles.kpi}>
-                    <span><EventIcon /></span>
-                    <div className={styles.content}>
-                        <h4>&#8358;0</h4>
-                        <p>Total Revenue</p>
-                    </div>
-                </div>
-            </div>
+            }
         </div>
     );
 }
