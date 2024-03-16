1599954f97afef071e75733fa1a87f9e911381d6
Simlex <simlexafol@gmail.com>
2024-03-16 11:12:16 +0100

    Updated model

--- app/models/ITicketOrder.ts
+++ app/models/ITicketOrder.ts
@@ -1,3 +1,6 @@
+import { EventVisibility } from "../enums/IEventVisibility";
+import { OrderStatus } from "../enums/IOrderStatus";
+
 export interface SingleTicketOrderRequest {
   ticketId: string;
   price: number;
@@ -11,3 +14,61 @@ export interface TicketOrderRequest {
   tickets: SingleTicketOrderRequest[];
   contactEmail: string;
 }
+
+//#region Responses
+
+export interface TicketEventOrderResponse {
+    id: string;
+    eventId: string;
+    publisherId: string;
+    title: string;
+    description: string;
+    locationId: null;
+    venue: string;
+    date: string;
+    time: string;
+    category: string;
+    visibility: EventVisibility;
+    mainImageUrl: string;
+    mainImageId: string;
+    currency: string;
+    purchaseStartDate: string;
+    purchaseEndDate: string;
+    allowedGuestType: string;
+    bookmarksCount: number;
+    favoritesCount: number;
+    ticketOrdersCount: number;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface TicketOrderResponse {
+  id: string;
+  eventId: string;
+  name: string;
+  price: string;
+  quantity: number;
+  remainingTickets: number;
+  numberOfUsers: number;
+  description: null;
+  ticketOrdersCount: number;
+  createdAt: string;
+  updatedAt: string;
+  event: TicketEventOrderResponse;
+}
+
+export interface UserTicketOrders {
+  id: string;
+  ticketId: string;
+  orderId: string;
+  associatedEmail: string;
+  contactEmail: string;
+  price: string;
+  orderStatus: OrderStatus;
+  paymentId: string;
+  createdAt: string;
+  updatedAt: string;
+  ticket: TicketOrderResponse;
+}
+
+//#endregion
