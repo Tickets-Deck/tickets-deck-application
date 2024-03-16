b95298799e5d252061a7daf4501d2588814c4315
Simlex <simlexafol@gmail.com>
2024-03-16 11:11:29 +0100

    Add serializer

---
+++ app/constants/serializer.ts
@@ -0,0 +1,16 @@
+import { OrderStatus } from "../enums/IOrderStatus";
+
+export function serializeOrderStatus(status: OrderStatus) {
+    switch (status) {
+        case OrderStatus.Pending:
+        return "Pending";
+        case OrderStatus.PaymentInitiated:
+        return "Initiated";
+        case OrderStatus.Confirmed:
+        return "Completed";
+        case OrderStatus.Cancelled:
+        return "Cancelled";
+        default:
+        return "Unknown";
+    }
+}
