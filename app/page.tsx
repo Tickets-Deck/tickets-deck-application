5b25297939d8780bfdc4bb30a2b8a441b42c33c7
Simlex <simlexafol@gmail.com>
2024-03-17 23:13:40 +0100

    Created homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-import styles from './styles/Home.module.scss'
-import HeroSection from './components/Homepage/HeroSection'
-import FeaturedEvents from './components/Homepage/FeaturedEvents'
-import Services from './components/Homepage/Services'
-import CreateEvent from './components/Homepage/CreateEvent'
+import Homepage from './Homepage'
 
 export default function Home() {
     return (
-        <>
-            <div className={styles.homepage}>
-                <HeroSection />
-                <FeaturedEvents />
-                <Services />
-                <CreateEvent />
-            </div>
-        </>
+        <Homepage />
     )
 }
