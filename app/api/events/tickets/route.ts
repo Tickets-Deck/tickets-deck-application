7acb8bcd0c9f1a41b3fb720b2d87dbc58e088cbf
Simlex <simlexafol@gmail.com>
2024-05-10 17:30:05 +0100

    Added method to update ticket information

--- app/api/events/tickets/route.ts
+++ app/api/events/tickets/route.ts
@@ -1,3 +1,4 @@
+import { TicketRequest, TicketResponse } from "@/app/models/ITicket";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -16,7 +17,7 @@ export async function GET(req: NextRequest) {
           eventId: specifiedEventId,
         },
         include: {
-            tickets: true,
+          tickets: true,
         },
       });
 
@@ -29,6 +30,64 @@ export async function GET(req: NextRequest) {
       }
 
       return NextResponse.json(event, { status: 200 });
+    }
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  if (req.method === "PUT") {
+    // Get the body from the request
+    const request = (await req.json()) as TicketResponse;
+
+    // Get the search params from the request url
+    const searchParams = new URLSearchParams(req.url.split("?")[1]);
+
+    // Get the eventId from the search params
+    // const specifiedEventId = searchParams.get("eventId");
+
+    // Get the ticketId from the search params
+    const specifiedTicketId = searchParams.get("ticketId");
+
+    // If a specifiedTicketId is provided, fetch the ticket with that ticketId
+    if (specifiedTicketId) {
+      const existingTicket = await prisma.tickets.findUnique({
+        where: {
+          id: specifiedTicketId,
+        },
+      });
+
+      if (!existingTicket) {
+        return NextResponse.json(
+          { error: "Ticket with specified Ticket ID not found" },
+          { status: 404 }
+        );
+      };
+
+    //   console.log("Request gotten: ", request);
+    //   console.log("test pr: ", request.price || existingTicket.price);
+
+      const ticket = await prisma.tickets.update({
+        where: {
+          id: specifiedTicketId,
+        },
+        data: {
+          // ...request
+          name: request.name || existingTicket.name,
+          price: request.price || existingTicket.price,
+          numberOfUsers: request.numberOfUsers || existingTicket.numberOfUsers,
+          quantity: request.quantity || existingTicket.quantity,
+          remainingTickets:
+            request.remainingTickets || existingTicket.remainingTickets,
+          description: request.description || existingTicket.description,
+        },
+      });
+
+      return NextResponse.json(ticket, { status: 200 });
+    } else {
+      return NextResponse.json(
+        { error: "Ticket ID not provided" },
+        { status: 400 }
+      );
     }
   }
 }
