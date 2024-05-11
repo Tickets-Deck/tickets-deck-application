79ff45f9e252882ae2133c516975f25a83a0c9d9
Simlex <simlexafol@gmail.com>
2024-05-11 09:29:56 +0100

    Added a deserializer for event visibility

--- app/constants/serializer.ts
+++ app/constants/serializer.ts
@@ -1,16 +1,28 @@
+import { EventVisibility } from "../enums/IEventVisibility";
 import { OrderStatus } from "../enums/IOrderStatus";
 
 export function serializeOrderStatus(status: OrderStatus) {
-    switch (status) {
-        case OrderStatus.Pending:
-        return "Pending";
-        case OrderStatus.PaymentInitiated:
-        return "Initiated";
-        case OrderStatus.Confirmed:
-        return "Completed";
-        case OrderStatus.Cancelled:
-        return "Cancelled";
-        default:
-        return "Unknown";
-    }
+  switch (status) {
+    case OrderStatus.Pending:
+      return "Pending";
+    case OrderStatus.PaymentInitiated:
+      return "Initiated";
+    case OrderStatus.Confirmed:
+      return "Completed";
+    case OrderStatus.Cancelled:
+      return "Cancelled";
+    default:
+      return "Unknown";
+  }
+}
+
+export function deserializeEventVisibility(status: string) {
+  switch (status) {
+    case "PUBLIC":
+      return EventVisibility.PUBLIC;
+    case "PRIVATE":
+      return EventVisibility.PRIVATE;
+    default:
+      return EventVisibility.PUBLIC;
+  }
 }
