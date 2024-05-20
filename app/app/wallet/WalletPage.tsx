f15df15ede47c2afbaeb416488ddf7c5094184dc
Simlex <simlexafol@gmail.com>
2024-05-20 11:51:19 +0100

    Created wallet page

---
+++ app/app/wallet/WalletPage.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent, ReactElement } from "react";
+
+interface WalletPageProps {
+    
+}
+ 
+const WalletPage: FunctionComponent<WalletPageProps> = ():ReactElement => {
+    return ( 
+        <main>
+            Wallet page
+        </main>
+     );
+}
+ 
+export default WalletPage;
