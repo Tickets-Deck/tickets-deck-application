1d0cd156920d9ff699c09bd5ec500376d2c10a87
Simlex <simlexafol@gmail.com>
2024-05-21 05:35:10 +0100

    Add application routes

---
+++ app/constants/applicationRoutes.ts
@@ -0,0 +1,84 @@
+/**
+ * Application routes
+ */
+export class ApplicationRoutes {
+  /**
+   * The route to the home page
+   */
+  static readonly Home = "/";
+
+  /**
+   * The route to the verify email page
+   */
+  static readonly VerifyEmail = "/auth/verify";
+
+  /**
+   * The route to the sign in page
+   */
+  static readonly SignIn = "/auth/signin";
+
+  /**
+   * The route to the sign up page
+   */
+  static readonly SignUp = "/auth/signup";
+
+  /**
+   * The route to the forgot password page
+   */
+  static readonly ForgotPassword = "/auth/forgot-password";
+
+  /**
+   * The route to the reset password page
+   */
+  static readonly ResetPassword = "/auth/reset-password";
+
+  /**
+   * The route to the general events page
+   */
+  static readonly GeneralEvents = "/events";
+
+  /**
+   * The route to the general event page
+   */
+  static readonly GeneralEvent = "/event";
+
+  /**
+   * The route to the verify payment page
+   */
+  static readonly VerifyPayment = "/verify-payment";
+
+  /**
+   * The route to the order page
+   */
+  static readonly Order = "/order";
+
+  /**
+   * The route to the user dashboard page
+   */
+  static readonly Dashboard = "/app";
+
+  /**
+   * The route to the profile page
+   */
+  static readonly Profile = "/app/profile";
+
+  /**
+   * The route to the wallet page
+   */
+  static readonly Wallet = "/app/wallet";
+
+  /**
+   * The route to the transactions page
+   */
+  static readonly Transactions = "/app/transactions";
+
+  /**
+   * The route to the events page
+   */
+  static readonly Events = "/app/events";
+
+  /**
+   * The route to the event page
+   */
+  static readonly Event = "/app/event";
+}
