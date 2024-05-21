5cdd6b1e75aae8d7b0df6e9a5db7e03e59d7be7f
Simlex <simlexafol@gmail.com>
2024-05-21 11:06:47 +0100

    Add storage keys for newly created user email

---
+++ app/constants/storageKeys.ts
@@ -0,0 +1,9 @@
+/**
+ * Storage keys used in the application
+ */
+export class StorageKeys {
+  /**
+   * Key for the newly created user's email
+   */
+  static readonly NewlyCreatedUserEmail = "newlyCreatedUserEmail";
+}
