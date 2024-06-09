a25cde491fdafd18036ae064742290293ac2ef10
Simlex <simlexafol@gmail.com>
2024-06-09 10:21:45 +0100

    Refactor Provider component to include AppProvider

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from 'next-auth/react'
 import userReducer from '@/app/redux/features/user/userSlice'
 import themeReducer from '@/app/redux/features/theme/themeSlice'
 import { Provider } from "react-redux";
+import { AppProvider } from '../context/ApplicationContext'
 
 export const store = configureStore({
     reducer: {
@@ -19,9 +20,11 @@ type Props = {
 
 export const GlobalProvider = ({ children }: Props) => {
 
-    return <Provider store={store}>
-        <SessionProvider>
-            {children}
-        </SessionProvider>
-    </Provider>
+    return <SessionProvider>
+        <AppProvider>
+            <Provider store={store}>
+                {children}
+            </Provider>
+        </AppProvider>
+    </SessionProvider>
 }
