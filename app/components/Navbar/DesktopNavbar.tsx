e6800b2c3071b9594ba9c8312bc35ab839eff23b
Simlex <simlexafol@gmail.com>
2024-06-09 10:26:52 +0100

    Update profile picture props in DesktopNavbar component

--- app/components/Navbar/DesktopNavbar.tsx
+++ app/components/Navbar/DesktopNavbar.tsx
@@ -57,7 +57,7 @@ const DesktopNavbar: FunctionComponent<DesktopNavbarProps> = ({ isLightTheme, se
                     {
                         user ? <>
                             <div className={styles.profileCircle}>
-                                {<Image src={user?.image ?? images.user_avatar} alt='Profile picture' fill /> ?? <UserIcon />}
+                                {<Image src={user?.image ?? images.user_avatar} alt='Profile picture' fill sizes="auto" /> ?? <UserIcon />}
                             </div>
                             <h3>{user?.name ?? 'Account'}</h3>
                             <span className={styles.dropdownIcon}>
