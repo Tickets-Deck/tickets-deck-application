82bb395e2c99a303b43fc56444cadd9fbb052890
Simlex <simlexafol@gmail.com>
2024-06-10 14:43:10 +0100

    Add SuccessfulActionModal component

---
+++ app/components/Modal/SuccessfulActionModal.tsx
@@ -0,0 +1,31 @@
+import { Dispatch, FunctionComponent, ReactElement, SetStateAction } from "react";
+import ModalWrapper from "./ModalWrapper";
+import styles from "@/app/styles/promptModal.module.scss";
+
+interface SuccessfulActionModalProps {
+    visibility: boolean
+    setVisibility: Dispatch<SetStateAction<boolean>>
+}
+
+const SuccessfulActionModal: FunctionComponent<SuccessfulActionModalProps> = ({ visibility, setVisibility }): ReactElement => {
+    return (
+        <ModalWrapper visibility={visibility} setVisibility={setVisibility} styles={{ backgroundColor: 'transparent', color: '#fff', width: "fit-content" }}>
+            {/* <div className={styles.loginPromptModal}>
+                <div className={styles.topAreaSection}>
+                    <div className={styles.topArea}>
+                        <h3>Hello there</h3>
+                        <p>You would need to login to perform this action</p>
+                    </div>
+                    <span className={styles.closeIcon} onClick={() => setVisibility(false)}><CloseIcon /></span>
+                </div>
+                <div className={styles.actionButton}>
+                    <Link href={ApplicationRoutes.SignIn} onClick={() => setVisibility(false)}>
+                        Login
+                    </Link>
+                </div>
+            </div> */}
+        </ModalWrapper>
+    );
+}
+
+export default SuccessfulActionModal;
