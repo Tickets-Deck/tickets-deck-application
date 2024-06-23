0e2c82ad3ef81958e380ff746eea81292d21803d
Simlex <simlexafol@gmail.com>
2024-06-23 18:14:43 +0100

    Add metadata to Homepage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
 import Homepage from './Homepage'
+
+export const metadata: Metadata = {
+    title: 'Homepage | Ticketsdeck Events',
+    description: 'Unlocking best experiences, easily.'
+}
 
 export default function Home() {
     return (
