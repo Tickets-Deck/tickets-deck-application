ecbc7712c36912cf0ba4f203fbc0b9a2bbd40d21
Simlex <simlexafol@gmail.com>
2024-08-13 08:34:46 +0100

    feat: update ticket order email template

--- lib/templates/ticketOrder.ts
+++ lib/templates/ticketOrder.ts
@@ -71,7 +71,7 @@ a[x-apple-data-detectors] {
                      <tr>
                       <td align="left" class="es-m-txt-c" style="padding:0;Margin:0;font-size:0px">
                         <a target="_blank" href="https://ticketsdeck.netlify.app" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:none;color:#3B8026;font-size:14px">
-                            <img src="https://ecqehch.stripocdn.email/content/guids/CABINET_6e6319abae3cda9440564f09b17d845e79f3612cdc02bd3c863444f15ee31c22/images/logo_white.png" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" title="Logo" width="87" height="40">
+                            <img src="https://res.cloudinary.com/dvxqk1487/image/upload/v1723533838/logo/etd-logo_gq8p9t.png" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" title="Logo" width="52" height="52">
                         </a>
                       </td>
                      </tr>
@@ -142,7 +142,7 @@ a[x-apple-data-detectors] {
                    <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                      <tr>
                       <td align="left" style="padding:0;Margin:0;padding-top:5px;padding-bottom:5px">
-                        <h3 style="Margin:0;line-height:24px;mso-line-height-rule:exactly;font-family:'Josefin Sans', helvetica, arial, sans-serif;font-size:18x;font-style:normal;font-weight:normal;color:#2D033A">
+                        <h3 style="Margin:0;line-height:24px;mso-line-height-rule:exactly;font-family:'Josefin Sans', helvetica, arial, sans-serif;font-size:22px;font-style:normal;font-weight:normal;color:#2D033A">
                             Event Information
                             <br><br>
                         </h3>
@@ -180,7 +180,7 @@ a[x-apple-data-detectors] {
                                 <br>-->
                         <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'Josefin Sans', helvetica, arial, sans-serif;line-height:21px;color:#38363A;font-size:14px">
                             Click the button below to view your ticket order
-                            <br><br>
+                            <br>
                         </p>
                     </td>
                      </tr>
