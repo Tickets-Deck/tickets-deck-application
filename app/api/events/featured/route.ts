aafba0c4c7f070a60c06e6b676462d977916016d
Simlex <simlexafol@gmail.com>
2024-08-16 17:31:49 +0100

    feat: add route for fetching featured events

---
+++ app/api/events/featured/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { validateRequestMethod } from "../../services/reusable-services/requestMethodValidator";
+import { customNextResponseError } from "../../utils/customNextResponseError";
+import { ApplicationError } from "@/app/constants/applicationError";
+import { StatusCodes } from "@/app/models/IStatusCodes";
+import { fetchFeaturedEvents } from "../../services/event/eventService";
+
+export async function GET(req: NextRequest) {
+  // validate request method
+  await validateRequestMethod(req, "GET");
+
+  try {
+    // Call the function to fetch events
+    const operation = await fetchFeaturedEvents(req);
+
+    // Return the response
+    return NextResponse.json(operation.data, {
+      status: StatusCodes.Success,
+    });
+  } catch {
+    // Return an error if the operation fails
+    return NextResponse.json(
+      { error: ApplicationError.InternalServerError.Text },
+      { status: StatusCodes.InternalServerError }
+    );
+  }
+}
