5f147ab5e6048b7f4f9d95560d7b5b75c41d3205
Simlex <simlexafol@gmail.com>
2024-08-18 22:44:03 +0100

    feat: add terms of use page

---
+++ app/terms-of-use/TermsPage.tsx
@@ -0,0 +1,214 @@
+import { FunctionComponent, ReactElement } from "react";
+
+interface TermsPageProps {
+
+}
+
+const TermsPage: FunctionComponent<TermsPageProps> = (): ReactElement => {
+    return (
+        <main className="px-5 md:px-40 py-8 min-h-screen bg-container-grey">
+            <div className="mb-3 text-white">
+                <h2 className="text-2xl font-medium mb-3">Terms Of Use</h2>
+                <p className="text-sm leading-6">
+                    These terms and conditions apply to You, the user of this website
+                    (including any sub-domains, unless expressly excluded by their own terms and conditions),
+                    and Ticketsdeck Solutions Limited, the owner and operator of the following website:
+                    https://ticketsdeck.com (the " website "), with the following URL web address:
+                    https://events.ticketsdeck.com.
+                </p>
+                <br />
+                <p>
+                    PLEASE READ THE TERMS AND CONDITIONS CAREFULLY as they affect your legal right.
+                </p>
+                <br />
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">1. Definitions</span>
+                    <p className="text-sm leading-6">
+                        "Parties" means both you (the user of the service) and the owner of this service.
+                        "Content" means any content, writing, images, audiovisual content or other information published on this service.
+                        "Materials" means any materials, information or documentation that we may provide to you in connection with your use of the products including documentation, data, information developed any user and other materials which may assist in your use of the service.
+
+                        "Terms" means terms and conditions.
+
+                        "Service" means the website including all pages, sub pages, all blogs, forums, and other connected internet content whatsoever.
+
+                        "Services" means the services offered on the website.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">2. About this Website</span>
+                    <p className="text-sm leading-6">
+                        The Website is an online store that engages in the sale of the following services:
+
+                        Developing, analyzing, implementing, operating, applying, evaluating and enhancing software programmes and enhancing software programmes and solutions and other services in information technology.
+
+                        This service is offered to you upon your acceptance of the Terms, conditions, notices hereinafter contained. Your use of this service constitutes Your agreement to all the terms contained herein.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">3. Agreement</span>
+                    <p className="text-sm leading-6">
+                        By using this Website, you acknowledge that you have reviewed, considered the terms of this agreement and understand same and agree to be bound by it. If you do not agree with these Terms or do not intend to be bound by it, you must quit the use of this website immediately. In addition, when using these services, you shall be subject to any posted guidelines or rules applicable to such services. Accordingly, any participation in this service shall constitute acceptance of this agreement.
+                        By using this Website and agreeing to these terms, you represent and warrant that you have the capacity to accept these terms.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">4. Acceptable Use</span>
+                    <p className="text-sm leading-6">
+                        We may provide you with other items in connection with your use of this service.
+                        We hereby grant you the license to use our service for your personal, non-commercial use to retrieve, display and view the content on a computer or mobile phone screen.
+                        The license created under these Terms is limited, non-exclusive, non-transferable and revocable.
+                        You agree that you will not use the content or materials for any other purpose which may be contrary to your license to use this service.
+                        Any unauthorized use by you shall terminate the permission or license granted by this website.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">5. Prohibited Use</span>
+                    <p className="text-sm leading-6 mb-2">
+                        You are expressly prohibited from collecting, downloading, copying or otherwise communicating with other users from the Website.
+                    </p>
+                    <p className="text-sm leading-6">You agree not to use the service in the following manner:</p>
+                    <ul className="text-sm leading-6 list-disc pl-3 mb-2">
+                        <li>To harass, abuse or threaten others or otherwise violate any person's legal rights;</li>
+                        <li>To perpetuate fraud;</li>
+                        <li>To create or transmit unnecessary spam to any person or URL;</li>
+                        <li>To post, transmit or cause to be posted or transmitted, any communication or solicitation designed to obtain password, account or private information of other users or persons;</li>
+                        <li>To post copyrighted content which does not belong to you and without the prior consent of the author;</li>
+                        <li>To use robot, spider, scraper or other automated means to access this service without obtaining the prior consent of the owner;</li>
+                        <li>To engage in or create any unlawful gambling, sweepstakes or scheme;</li>
+                        <li>Publishing or distributing any obscene or defamatory material;</li>
+                        <li>Using this service in any way that impacts user access to the website;</li>
+                        <li>To engage in advertisement or solicit any user to buy or sell products or services without obtaining the prior consent of the owner;</li>
+                        <li>Disseminating computer viruses or other software;</li>
+                        <li>Violating any intellectual property rights of the owner or any third party;</li>
+                        <li>To use the website or any of the services for illegal spam activities.</li>
+                    </ul>
+                    <p className="text-sm leading-6">
+                        Additionally, you agree that you not do as follows:
+                    </p>
+                    <ul className="text-sm leading-6 list-disc pl-3 mb-2">
+                        <li>Interfere or attempt to interfere with the proper working of this website; or</li>
+                        <li>bypass any measures we may use to prevent or restrict access to this website;</li>
+                        <li>To interfere with or circumvent security features of this service;</li>
+                        <li>To damage, disable, overburden or impair service or any other person's use of this service.</li>
+                        <li>To use this service contrary to the applicable laws and regulations or in any way that causes, or may cause harm to this website, any person or business entity. We reserve the right to terminate your use of the service for violating any of the prohibited uses.</li>
+                    </ul>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">6. Intellectual Property Ownership</span>
+                    <p className="text-sm leading-6">
+                        You agree that we retain ownership of all content included on the website (test, graphics, video, software, data, page layout, images and any other information capable of being stored in a computer) other than the content uploaded by users
+                        You are granted a limited license only subject to the restrictions provided in these terms, nothing on this website shall be construed as granting any license or right to use any trademark or logo displayed on the website without obtaining the prior written consent of the owner.
+                        You hereby agree not to reproduce or distribute the owner's intellectual property or use the intellectual property for any unlawful purpose.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">7. Sale of Goods/Services</span>
+                    <p className="text-sm leading-6">
+                        The Website may offer products or service for sale. The owner undertakes to give accurate information about the description of the product and services. However, the owner does not guarantee the reliability of any information relating to the products and services.
+                        We reserve the right to refuse to sell the services provided on the website at our sole discretion.
+                        Note that there is no refund/replacement policy as the services provided are sold "As it is."
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">8. Payment and Billing</span>
+                    <p className="text-sm leading-6">
+                        If you register for our service or to purchase any service offered on this website. You agree to pay the full price for the services when the purchase is made.
+                        The total price will also include the taxes applicable on the date of purchase.
+                        The total price of the services provided including all applicable taxes is included upon the confirmation of your order.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">9. Performance of Services</span>
+                    <p className="text-sm leading-6">
+                        Upon payment for our services, we may offer the opportunity to book a time and date for the performance of the services. The services will be performed within a reasonable time.
+                        If you have any questions regarding the time and date for the performance, contact us.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">10. Privacy Policy</span>
+                    <p className="text-sm leading-6">
+                        Our privacy policy explains how we treat your personal data and protect your privacy when you use our service. By using our service, you agree that the owner can use such data in the manner described in our privacy policy.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">11. Links to Third Party Sites/Services</span>
+                    <p className="text-sm leading-6">
+                        The website may contain links to other websites. The linked sites are not under our control and you agree that we are not liable for the contents of any linked site or link contained in any linked site.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">12. Electronic Communications</span>
+                    <p className="text-sm leading-6">
+                        You consent to receive electronic communications and you agree that all agreements, notices, disclosure and other communications we provide to you electronically, via email and on this website, satisfy any legal requirements that communications must be in writing.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">13. Reverse Engineering and Security</span>
+                    <p className="text-sm leading-6">
+                        You hereby agree as follows:
+                    </p>
+                    <ul className="text-sm leading-6 list-disc pl-3 mb-2">
+                        <li>not to reverse engineer or permit the reverse engineering or dissemble any code or software from or on the website or services; and</li>
+                        <li>not to violate the security of the website or other services through any unauthorized access, circumvention of encryption or other security tools, data mining or interferences with any host or user or network.</li>
+                    </ul>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">14. Change to service</span>
+                    <p className="text-sm leading-6">
+                        You accept that the owner may vary, alter, amend or update the content or service, services at any time and without your consent.
+                        You also agree that the services may not be available at all times and this may be as a result of the maintenance or for any other reason and we shall not be held liable for the failure to provide this service.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">15. INDEMNIFICATION</span>
+                    <p className="text-sm leading-6">
+                        You hereby agree to indemnify the owner,  its employees, agents and third parties from and against all liabilities, cost, demands, cause of action, damages; and expenses (including reasonable legal fees arising out of your inability to use, your violation of any rights of a third party and your violation of applicable laws, rules or regulation.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">16. No Warranties</span>
+                    <p className="text-sm leading-6">
+                        You agree that you use this website solely at your risk as the owner does not warrant the accuracy of the contents in this website. You assume all the risk of viewing, reading, or downloading the contents of this website.
+
+                        The owner expressly disclaims all express and implied warranties such as implied merchantability as the owner makes no warranties that the websites or other services will be accurate, error free, secure or uninterrupted.
+
+                        The owner makes no warranty about the suitability, reliability, availability, timeliness and the accuracy of the information, contents, services and other materials contained herein for any purpose. The owner hereby disclaims all warranties and conditions with regard to the information, software, products, related graphics and materials, including all implied warranties or conditions of merchantability, fitness for a particular purpose, title, and non-infringement.
+
+                        You also agree that the owner and its affiliates shall not be liable for any direct, indirect, punitive and all consequential damages or any damages whatsoever including, but not limited to damages for loss of use, data or profits, the failure to provide services or for any information, software, products, services, related graphics and materials obtained through this website, or otherwise arising out of the use of this website, whether based on contracts, negligence, strict liability, or otherwise.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">17. Service Interruptions</span>
+                    <p className="text-sm leading-6">
+                        The owner may from time to time interrupt your access or use of this website to perform some maintenance or emergency services and you agree that the owner shall not be held liable for any damage loss which may arise thereof.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">18. Termination/Restriction of Access</span>
+                    <p className="text-sm leading-6">
+                        The owner reserves the right to, as its sole discretion, terminate your access to this website and related service or any parts thereof at any time, for any reason and without notice.
+
+                        The owner has the right to terminate or terminate / suspend your account for violating the terms of this service.
+
+                        If you register with us, you may terminate this service at any time by issuing a prior notice to us. Once this is done, you will no longer be bound by the provisions of this terms.
+                    </p>
+                </div>
+                <div className="mb-2">
+                    <span className="font-Mona-Sans-Wide text-primary-color-sub">19. General Provisions</span>
+                    <p className="text-sm leading-6">
+                        Assignment: The owner shall be permitted to assign, transfer its rights and/or obligations under these Terms. However, you shall not be permitted to assign, transfer any rights and/or obligations under this Terms.
+
+                        Entire agreement: These Terms, disclaimers and any other agreement and shall supersede any other agreement.
+
+                        Separate Agreements: You may have other legal agreements with us. Those agreements are separate from these terms. These terms are not intended to alter, amend, revise or replace the terms of the other agreement(s).
+
+                        Applicable law: These terms may be governed and construed in accordance with the laws, regulations or guidelines of the federal republic of Nigeria and other treaties, or regulations which is applicable in Nigeria.
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default TermsPage;
---
+++ app/terms-of-use/page.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent, ReactElement } from "react";
+import TermsPage from "./TermsPage";
+
+interface TermsOfUseProps {
+    
+}
+ 
+const TermsOfUse: FunctionComponent<TermsOfUseProps> = ():ReactElement => {
+    return ( 
+        <TermsPage />
+     );
+}
+ 
+export default TermsOfUse;
