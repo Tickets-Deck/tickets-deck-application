44b7234c2bb8d5f4a509100ddf11697511566b91
Simlex <simlexafol@gmail.com>
2024-08-20 13:11:07 +0100

    feat: add timePickerStyles to app/styles

---
+++ app/styles/timePicker.tsx
@@ -0,0 +1,19 @@
+import { IComboBoxStyles } from "@fluentui/react";
+
+export const timePickerStyles: Partial<IComboBoxStyles> = {
+    optionsContainerWrapper: {
+        height: '80vh',
+    },
+    root: {
+        width: '100%',
+        height: '43px',
+        borderRadius: '10px',
+        padding: '0.5rem 1rem',
+        outline: 'none',
+        borderStyle: 'none',
+        borderColor: 'transparent',
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        color: 'white',
+    },
+
+};
