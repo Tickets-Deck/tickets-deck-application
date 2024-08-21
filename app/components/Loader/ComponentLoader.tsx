f59495f91f87f2e01b88748098e75032529e5fa2
Simlex <simlexafol@gmail.com>
2024-08-21 18:12:48 +0100

    feat: add ButtonLoader component

--- app/components/Loader/ComponentLoader.tsx
+++ app/components/Loader/ComponentLoader.tsx
@@ -46,3 +46,23 @@ const ComponentLoader: FunctionComponent<ComponentLoaderProps> = (
 }
 
 export default ComponentLoader;
+
+
+
+type ButtonLoaderProps = {
+
+    /**
+     * The optional classnames to be added
+     */
+    className?: string
+}
+
+export const ButtonLoader: FunctionComponent<ButtonLoaderProps> = (
+    { className }): ReactElement => {
+
+    return (
+        <div className='absolute w-full h-full top-0 left-0 bg-primary grid place-items-center pointer-events-none'>
+            <div className={`w-6 h-6 border-4 border-white border-t-transparent border-solid rounded-full animate-spin ${className}`} />
+        </div>
+    );
+}
