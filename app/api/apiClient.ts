2f7516e9c21007e95564be4cbaf47f43b33dbaa7
Simlex <simlexafol@gmail.com>
2024-08-31 13:21:35 +0100

    Update API base URL to test environment

--- app/api/apiClient.ts
+++ app/api/apiClient.ts
@@ -21,7 +21,7 @@ import {
 import { BankAccount, BankAccountDetailsRequest } from "../models/IBankAccount";
 
 export const API = axios.create({
-  baseURL: ApiRoutes.BASE_URL_LIVE,
+  baseURL: ApiRoutes.BASE_URL_TEST,
 });
 
 export function useCreateNewsletterSubscriber() {
