ad339edd3dc38abe628382df9a411233ef2b1a8c
Simlex <simlexafol@gmail.com>
2024-08-31 13:19:33 +0100

    Fix ticket quantity validation error

--- app/components/Event/Create/TicketsCreation/TicketCreationModal.tsx
+++ app/components/Event/Create/TicketsCreation/TicketCreationModal.tsx
@@ -346,8 +346,8 @@ const TicketCreationModal: FunctionComponent<TicketCreationModalProps> = (
                                 onFormValueChange(e, setTicketQuantityErrorMsg);
                             }}
                             onKeyUp={(e) => {
-                                if (Number(ticketFormRequest?.quantity) < 1 && e.currentTarget.value != "") {
-                                    setTicketQuantityErrorMsg({ message: "The minimum number of tickets is 1", status: DefaultFormResponseStatus.Failed });
+                                if (Number(ticketFormRequest?.quantity) < 0 && e.currentTarget.value != "") {
+                                    setTicketQuantityErrorMsg({ message: "The minimum number of available tickets cannot be less than 0", status: DefaultFormResponseStatus.Failed });
                                 }
                             }}
                             onKeyDown={(e) => {
