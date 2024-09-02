b34287993884bd5a93c83e64519ad61cb73d623d
Simlex <simlexafol@gmail.com>
2024-09-02 16:15:52 +0100

    refactor: Update EventDetailsPage to fetch event details by ID and generate metadata

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,11 +1,39 @@
-"use client"
 import { FunctionComponent, ReactElement } from 'react';
 import EventDetailsPage from './EventInfoPage';
 import { EventResponse } from '@/app/models/IEvents';
-import { useFetchEvents } from '@/app/api/apiClient';
+import { useFetchEventById, useFetchEvents } from '@/app/api/apiClient';
+import { Metadata, ResolvingMetadata } from 'next';
 
 interface EventDetailsProps {
     params: { id: string }
+}
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    // read route params
+    const id = params.id
+
+    const fetchEventInfo = useFetchEventById();
+
+    // fetch data
+    const event = await fetchEventInfo(id).then((response) => response.data as EventResponse);
+
+    // optionally access and extend (rather than replace) parent metadata
+    const previousImages = (await parent).openGraph?.images || []
+
+    return {
+        title: event.title,
+        openGraph: {
+            images: [event.mainImageUrl, ...previousImages],
+        },
+    }
 }
 
 
