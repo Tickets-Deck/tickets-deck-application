69f9bd6b0178e807f7e9868e9cdeb812c7f03be1
Simlex <simlexafol@gmail.com>
2024-09-03 08:07:16 +0100

    refactor: Update API base URL to live environment

--- app/api/apiClient.ts
+++ app/api/apiClient.ts
@@ -21,7 +21,7 @@ import {
 import { BankAccount, BankAccountDetailsRequest } from "../models/IBankAccount";
 
 export const API = axios.create({
-  baseURL: ApiRoutes.BASE_URL_TEST,
+  baseURL: ApiRoutes.BASE_URL_LIVE,
 });
 
 export function useCreateNewsletterSubscriber() {
