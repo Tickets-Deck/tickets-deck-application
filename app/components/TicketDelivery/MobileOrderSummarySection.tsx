22ae2d6c76a51c7c6a01a80399bf7dcb16a7819b
Simlex <simlexafol@gmail.com>
2024-09-13 17:43:52 +0100

    feat: Integrate coupon discount and transaction fees calculation in MobileOrderSummarySection

--- app/components/TicketDelivery/MobileOrderSummarySection.tsx
+++ app/components/TicketDelivery/MobileOrderSummarySection.tsx
@@ -1,19 +1,35 @@
 import moment from "moment";
 import Image from "next/image";
-import { FunctionComponent, ReactElement } from "react";
+import { FunctionComponent, ReactElement, useContext } from "react";
 import styles from "@/app/styles/TicketDelivery.module.scss";
 import { RetrievedTicketResponse } from "@/app/models/ITicket";
 import { EventResponse } from "@/app/models/IEvents";
+import { CouponDetails } from "@/app/models/ICoupon";
+import { ApplicationContext, ApplicationContextData } from "@/app/context/ApplicationContext";
 
 interface MobileOrderSummarySectionProps {
     eventTickets: RetrievedTicketResponse[] | undefined
     totalPrice: number
     eventInfo: EventResponse | undefined
+    couponDetails: CouponDetails | undefined
 }
 
 const MobileOrderSummarySection: FunctionComponent<MobileOrderSummarySectionProps> = ({
-    eventInfo, eventTickets, totalPrice
+    eventInfo, eventTickets, totalPrice, couponDetails
 }): ReactElement => {
+    
+
+    const { transactionFees } = useContext(ApplicationContext) as ApplicationContextData;
+
+    const eventTransactionFee = transactionFees?.find((transactionFee) => transactionFee.events.find((event) => event.title == eventInfo?.title));
+    const generalTransactionFee = transactionFees?.find((transactionFee) => transactionFee.events.length == 0);
+
+    const transactionFeePercentage = Number(eventTransactionFee?.percentage as string) || Number(generalTransactionFee?.percentage as string) || 0;
+    const flatFee = Number(eventTransactionFee?.flatFee as string) || Number(generalTransactionFee?.flatFee as string) || 0; 
+
+    const fees = (transactionFeePercentage ? (totalPrice * transactionFeePercentage) / 100 : 0) + (flatFee || 0);
+    const totalAmountPayable = totalPrice + fees - (couponDetails?.discount ? (totalPrice * Number(couponDetails.discount)) / 100 : 0);
+
     return (
         <>
             <div className={styles.eventImageContainer}>
@@ -42,8 +58,12 @@ const MobileOrderSummarySection: FunctionComponent<MobileOrderSummarySectionProp
                     <span className={styles.value}>&#8358;{(totalPrice)?.toLocaleString()}</span>
                 </div>
                 <div className={styles.summaryInfo__subs}>
-                    <span>Discount (0% off)</span>
-                    <span className={styles.value}>-&nbsp;&#8358;{(0).toLocaleString()}</span>
+                    <span>Discount ({couponDetails?.discount ?? 0}% off)</span>
+                    <span className={styles.value}>-&nbsp;&#8358;{((totalPrice * Number(couponDetails?.discount ?? 0)) / 100).toLocaleString()}</span>
+                </div>
+                <div className={styles.summaryInfo__subs}>
+                    <span>Fees</span>
+                    <span className={styles.value}>&#8358;{(fees)?.toLocaleString()}</span>
                 </div>
                 <div className={styles.summaryInfo__subs}>
                     <span>Total</span>
