3b89413350d4b92d129a144cb62c20eef5dd63d6
Simlex <simlexafol@gmail.com>
2024-10-04 09:37:26 +0100

    feat: modify tickets sold function to show only confirmed orders

--- app/api/services/ticket/ticketService.ts
+++ app/api/services/ticket/ticketService.ts
@@ -1,4 +1,5 @@
 import { ApplicationError } from "@/app/constants/applicationError";
+import { OrderStatus } from "@/app/enums/IOrderStatus";
 import { TicketCategory } from "@/app/enums/ITicket";
 import { StatusCodes } from "@/app/models/IStatusCodes";
 import { TicketRequest, TicketResponse } from "@/app/models/ITicket";
@@ -85,46 +86,12 @@ export async function fetchUserTickets(req: NextRequest) {
         publisherId: specifiedUserId,
       },
       include: {
-        tickets: true,
+        tickets: true
       },
       orderBy: {
         createdAt: "desc",
       },
     });
-
-    // Fetch all ordered tickets of every ticket order of each event of the user
-    // const soldTickets = await prisma.orderedTickets.findMany({
-    //   where: {
-    //     ticket: {
-    //       eventId: {
-    //         in: events.map((event) => event.id),
-    //       },
-    //     },
-    //   },
-    //   orderBy: {
-    //     createdAt: "desc",
-    //   },
-    // });
-
-    // let soldTicketsWithEvent = [];
-
-    // // Fetch the information for the ticket of each ordered ticket
-    // for (let i = 0; i < soldTickets.length; i++) {
-    //   const ticket = await prisma.tickets.findFirst({
-    //     where: {
-    //       id: soldTickets[i].ticketId,
-    //     },
-    //     include: {
-    //       event: true,
-    //     },
-    //   });
-
-    //   // Replace the ticket object with the ticket information
-    //   soldTicketsWithEvent.push({
-    //     ...soldTickets[i],
-    //     ticket: ticket,
-    //   });
-    // }
 
     // Extract all ticket IDs from the fetched events
     const eventTicketIds = events.flatMap((event) =>
@@ -136,6 +103,9 @@ export async function fetchUserTickets(req: NextRequest) {
       where: {
         ticketId: {
           in: eventTicketIds,
+        },
+        orderStatus: {
+            equals: OrderStatus.Confirmed,
         },
       },
       include: {
