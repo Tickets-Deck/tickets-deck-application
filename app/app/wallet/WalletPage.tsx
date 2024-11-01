215db679a027ed413eb4ed0a8dd5613b0ec23fcb
Simlex <simlexafol@gmail.com>
2024-11-01 17:52:01 +0100

    feat: implement user profile update functionality for payouts

--- app/app/wallet/WalletPage.tsx
+++ app/app/wallet/WalletPage.tsx
@@ -1,10 +1,13 @@
 "use client"
-import { useFetchUserWalletBalance } from "@/app/api/apiClient";
+import { useFetchUserPayouts, useFetchUserWalletBalance, useInitiateWitdrawal } from "@/app/api/apiClient";
 import BetaTestModal from "@/app/components/Modal/BetaTestModal";
+import WitdrawalModal from "@/app/components/Modal/WitdrawalModal";
 import { catchError } from "@/app/constants/catchError";
 import { OrderStatus } from "@/app/enums/IOrderStatus";
+import { Payout } from "@/app/models/IWallet";
 import { RootState } from "@/app/redux/store";
-import { FunctionComponent, ReactElement, useMemo, useState } from "react";
+import moment from "moment";
+import { FunctionComponent, ReactElement, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { toast } from "sonner";
 
@@ -15,11 +18,19 @@ interface WalletPageProps {
 const WalletPage: FunctionComponent<WalletPageProps> = (): ReactElement => {
 
     const fetchUserWalletBalance = useFetchUserWalletBalance();
-    const userInfo = useSelector((state: RootState) => state.userCredentials.userInfo);
+    const fetchUserPayouts = useFetchUserPayouts();
+    const initiateWitdrawal = useInitiateWitdrawal();
 
-    const [showBetaTestModal, setShowBetaTestModal] = useState(false);
+    const userInfo = useSelector((state: RootState) => state.userCredentials.userInfo);
+    const userBankAccount = useSelector((state: RootState) => state.wallet.bankAccount);
+
+    const [userPayouts, setUserPayouts] = useState<Payout[]>();
+    const [isFetchingPayouts, setIsFetchingPayouts] = useState(false);
     const [walletBalance, setWalletBalance] = useState<number>();
-    const [showWalletBalance, setShowWalletBalance] = useState(false);
+    const [showWalletBalance, setShowWalletBalance] = useState(true);
+    const [showWithdrawalModal, setShowWithdrawalModal] = useState(false);
+    const [isProcessingWithdrawal, setIsProcessingWithdrawal] = useState(false);
+    const [withdrawalAmount, setWithdrawalAmount] = useState<number>();
 
     const handleFetchUserWalletBalance = async () => {
         await fetchUserWalletBalance(userInfo?.id as string)
@@ -30,6 +41,46 @@ const WalletPage: FunctionComponent<WalletPageProps> = (): ReactElement => {
                 catchError(error);
                 toast.error("Failed to fetch user wallet balance");
             });
+    };
+
+    const handleInitiateWithdrawal = async () => {
+        if (!withdrawalAmount) {
+            toast.error("Please enter the amount to withdraw");
+            return
+        }
+        if (withdrawalAmount < 5000) {
+            toast.error("Minimum withdrawal amount is 5000 naira");
+            return
+        }
+
+        setIsProcessingWithdrawal(true);
+
+        await initiateWitdrawal(userInfo?.id as string, withdrawalAmount.toString())
+            .then(async (response) => {
+                await handleFetchUserPayouts();
+                toast.success("Withdrawal initiated successfully");
+                setShowWithdrawalModal(false);
+            })
+            .catch((error) => {
+                catchError(error);
+                toast.error("Failed to initiate withdrawal");
+            })
+            .finally(() => {
+                setIsProcessingWithdrawal(false);
+            })
+    };
+
+    const handleFetchUserPayouts = async () => {
+        await fetchUserPayouts(userInfo?.id as string)
+            .then((response) => {
+                setUserPayouts(response.data);
+            })
+            .catch((error) => {
+                console.log("🚀 ~ handleFetchUserPayouts ~ error:", error)
+            })
+            .finally(() => {
+                setIsFetchingPayouts(false);
+            })
     };
 
     // function showTagStyle(orderStatus: OrderStatus) {
@@ -48,68 +99,109 @@ const WalletPage: FunctionComponent<WalletPageProps> = (): ReactElement => {
     // }
 
     useMemo(() => {
-        if (userInfo)
+        if (userInfo) {
             handleFetchUserWalletBalance();
+            handleFetchUserPayouts();
+        }
     }, [userInfo]);
+
+    useEffect(() => {
+        if (!showWithdrawalModal) {
+            setWithdrawalAmount(undefined);
+        }
+    }, [showWithdrawalModal])
 
     return (
         <>
-            <BetaTestModal
-                visibility={showBetaTestModal}
-                setVisibility={setShowBetaTestModal}
+            <WitdrawalModal
+                visibility={showWithdrawalModal}
+                setVisibility={setShowWithdrawalModal}
+                isBankAccountAdded={userBankAccount?.length !== 0}
+                isProcessingWithdrawal={isProcessingWithdrawal}
+                withdrawalAmount={withdrawalAmount}
+                setWithdrawalAmount={setWithdrawalAmount}
+                handleInitiateWithdrawal={handleInitiateWithdrawal}
             />
             <main className="px-4 py-8 min-h-screen">
                 <div className="mb-4 flex flex-row justify-between w-full">
                     <h2 className="text-2xl font-medium text-gray-300">Wallet page</h2>
-                    <button className="w-fit h-fit rounded-full cursor-pointer text-sm p-4 py-2 border-none bg-primary-color text-white hover:bg-white hover:text-primary-color" onClick={() => setShowBetaTestModal(true)}>Request payment</button>
+                    <button
+                        className="w-fit h-fit rounded-full cursor-pointer text-sm p-4 py-2 border-none bg-primary-color text-white hover:bg-white hover:text-primary-color"
+                        onClick={() => setShowWithdrawalModal(true)}>
+                        Request payment
+                    </button>
                 </div>
 
                 {/* KPI Section */}
                 <div className="w-full grid grid-cols-2 gap-4">
                     <div className="p-6 rounded-2xl flex flex-col bg-dark-grey">
-                        <span className="text-2xl font-semibold mb-4 font-Mona-Sans-Wide">&#8358;{walletBalance && (walletBalance).toLocaleString()}</span>
-                        <span className="text-sm font-light text-text-grey">Wallet Balance</span>
+                        <span className="text-2xl font-semibold mb-4 font-Mona-Sans-Wide">&#8358;{walletBalance && showWalletBalance ? (walletBalance).toLocaleString() : "****"}</span>
+                        <div className="flex flex-row gap-2">
+                            <span className="text-sm font-light text-text-grey">Wallet Balance</span>
+                            <button
+                                onClick={() => setShowWalletBalance(!showWalletBalance)}
+                                className="text-sm font-light text-text-grey/80">
+                                {showWalletBalance ? "Hide" : "Show"}
+                            </button>
+                        </div>
                     </div>
                     <div className="p-6 rounded-2xl flex flex-col bg-dark-grey">
                         <span className="text-2xl font-semibold mb-4 font-Mona-Sans-Wide">&#8358;0</span>
                         <span className="text-sm font-light text-text-grey">Total Witdrawals</span>
                     </div>
                 </div>
-                {/* <div className="flex mt-4">
-                </div> */}
-
 
                 {
-                    false &&
+                    userBankAccount &&
+                    <div className="mt-5 flex flex-col gap-2">
+                        <h4>Bank details</h4>
+                        <div className="bg-text-grey/30 p-5 rounded-2xl w-fit">
+                            <div>
+                                <h4>Bank name: {userBankAccount[0].bankName}</h4>
+                                <h4>Account number: {userBankAccount[0].accountNumber}</h4>
+                                <h4>Account name: {userBankAccount[0].accountName}</h4>
+                            </div>
+                        </div>
+                    </div>
+                }
+
+                {
+                    !isFetchingPayouts && userPayouts &&
                     <div className="mt-6 w-full">
-                        <h3 className="mb-1 text-xl">Payouts</h3>
+                        <h3 className="mb-3 text-xl">Payouts</h3>
                         <div className="max-h-[65vh] w-full overflow-y-auto rounded-2xl relative bg-container-grey">
                             <table className="bg-white w-full text-dark-grey">
                                 <tbody>
                                     <tr>
                                         <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Transaction Reference</th>
                                         <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Amount</th>
-                                        <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Date</th>
+                                        <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Date Requested</th>
+                                        <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Date Paid</th>
                                         <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Status</th>
                                         <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Payment Method</th>
                                         <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Service Fee</th>
                                         <th className="p-3 border-b-[1px] text-left text-sm font-semibold">Tax</th>
                                     </tr>
 
-                                    <tr>
-                                        <td className="p-3 border-b-[1px] text-left text-sm">1722343534-sdwerndfy</td>
-                                        <td className="p-3 border-b-[1px] text-left text-sm">&#8358;450,000</td>
-                                        <td className="p-3 border-b-[1px] text-left text-sm">Mon 15th Jan, 2024 | 04:25pm</td>
-                                        <td className="p-3 border-b-[1px] text-left text-sm">Paid</td>
-                                        <td className="p-3 border-b-[1px] text-left text-sm">Bank Transfer</td>
-                                        <td className="p-3 border-b-[1px] text-left text-sm">5%</td>
-                                        <td className="p-3 border-b-[1px] text-left text-sm">
-                                            {/* <span className={showTagStyle(OrderStatus.Pending)}> */}
-                                            <span className="bg-success-color w-fit px-2 py-4 rounded-xl text-white text-lg">
-                                                5%
-                                            </span>
-                                        </td>
-                                    </tr>
+                                    {
+                                        userPayouts.map((payout, index) =>
+                                            <tr key={index}>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">{payout.transactionRef ?? "Unavailable"}</td>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">&#8358;{payout.amount}</td>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">{moment(payout.createdAt).format("Do MMM, YYYY | hh:mma")}</td>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">{payout.payoutDate ? moment(payout.payoutDate).format("Do MMM, YYYY | hh:mma") : "Unavailable"}</td>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">{payout.status}</td>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">{payout.paymentMethod}</td>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">{`${payout.serviceFees ?? 0}%`}</td>
+                                                <td className="p-3 border-b-[1px] text-left text-sm">{`${payout.tax ?? 0}%`}</td>
+                                                {/* <td className="p-3 border-b-[1px] text-left text-sm">
+                                                    <span className="bg-success-color w-fit px-2 py-4 rounded-xl text-white text-lg">
+                                                        5%
+                                                    </span>
+                                                </td> */}
+                                            </tr>
+                                        )
+                                    }
                                     {/* <tr key={index}>
                                             <td>{userRecentTransaction.eventName}</td>
                                             <td>{userRecentTransaction.orderId}</td>
