e62f8c8777a07f0e5266f6909900d86b156568f7
Simlex <simlexafol@gmail.com>
2024-11-01 17:50:49 +0100

    feat: implement API key check and refactor authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,47 +1,12 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 import { withAuth } from "next-auth/middleware";
+import { NextRequest } from "next/server";
+import { checkApiKeyMiddleware } from "./app/middleware/checkApiKeyMiddleware";
 
-// This function can be marked `async` if using `await` inside
-// export function middleware(request: NextRequest) {
-//   const devUserIsAuthenticated = request.cookies.get("next-auth.session-token");
-//   const userIsAuthenticated = request.cookies.get("__Secure-next-auth.session-token");
-
-//     if(request.nextUrl.pathname.startsWith("/app")) {
-//         if(!devUserIsAuthenticated || !userIsAuthenticated) {
-//             return NextResponse.redirect(new URL("/auth/signin", request.url));
-//         }
-//         // Otherwise, let the request continue
-//         return NextResponse.next();
-//     } else {
-//         return NextResponse.next();
-//     }
-
-// If the user is not authenticated, redirect to /auth/signin
-// if ((!devUserIsAuthenticated || !userIsAuthenticated) && request.nextUrl.pathname.startsWith("/app")) {
-//     return NextResponse.redirect(new URL("/auth/signin", request.url));
-// } else {
-//     // Otherwise, let the request continue
-//     return NextResponse.next();
-// }
-
-//   if (request.nextUrl.pathname.startsWith("/app")) {
-//     // This logic is only applied to pages that start with /app
-//     return NextResponse.redirect(new URL("/auth/signin", request.url));
-//   } else {
-//     // Otherwise, let the request continue
-//     return NextResponse.next();
-//   }
-// }
-
-export default withAuth({
+// Wrap the `withAuth` middleware
+const authMiddleware = withAuth({
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     authorized: ({ req, token }) => {
-      // verify token and return a boolean
-      //   const sessionToken = req.cookies.get("next-auth.session-token");
-      //   if (sessionToken) return true;
-      //   else return false;
       if (token === null) return false;
       else return true;
     },
@@ -51,9 +16,33 @@ export default withAuth({
   },
 });
 
+export function middleware(req: NextRequest) {
+  console.log("🚀 ~ middleware ~ req.nextUrl.pathname:", req.nextUrl.pathname);
+  // Apply the API key check for `/api` routes
+  if (req.nextUrl.pathname.startsWith("/api")  && !req.nextUrl.pathname.startsWith("/api/auth")) {
+    const apiKeyResponse = checkApiKeyMiddleware(req);
+    if (apiKeyResponse) return apiKeyResponse;
+  }
+
+  withAuth({
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+      authorized: ({ req, token }) => {
+        if (token === null) return false;
+        else return true;
+      },
+    },
+    pages: {
+      signIn: "/auth/signin",
+    },
+  });
+  // Apply `withAuth` for other routes (e.g., `/app`)
+  // return authMiddleware(req);
+}
+
 // See "Matching Paths" below to learn more
 export const config = {
-//   matcher: "/app/:path*",
+  //   matcher: "/app/:path*",
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
@@ -66,7 +55,7 @@ export const config = {
     // "/((?!auth|_next/static|_next/image|favicon.ico|api|$).*)", // Uncomment to use in postman
     // Explicitly include every path under /app
     "/app/:path*",
-    "/api/auth"
+    "/api/:path*",
     // Use regex to match patterns
     // "/path-prefix/(.*)",
   ],
