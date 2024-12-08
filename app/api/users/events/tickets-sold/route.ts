725f9b75de6a968db3f598cd9f7d0adca98a4251
Simlex <simlexafol@gmail.com>
2024-12-08 11:53:23 +0100

    feat: add GET route for fetching tickets sold for an event

---
+++ app/api/users/events/tickets-sold/route.ts
@@ -0,0 +1,30 @@
+import { StatusCodes } from "@/app/models/IStatusCodes";
+import { NextRequest, NextResponse } from "next/server";
+import { customNextResponseError } from "../../../utils/customNextResponseError";
+import { validateRequestMethod } from "../../../services/reusable-services/requestMethodValidator";
+import { ApplicationError } from "@/app/constants/applicationError";
+import { fetchTicketsSoldForEvent } from "../../../services/user/ticketService";
+
+export async function GET(req: NextRequest) {
+  // Call the request validation method
+  await validateRequestMethod(req, "GET");
+
+  try {
+    // Call the function to fetch tickets
+    const operation = await fetchTicketsSoldForEvent(req);
+
+    // If the operation fails, return an error
+    if (operation.error) {
+      return customNextResponseError(operation);
+    }
+
+    // Return the response
+    return NextResponse.json(operation.data, { status: StatusCodes.Success });
+  } catch {
+    // Return an error if the operation fails
+    return NextResponse.json(
+      { error: ApplicationError.FailedToFetchTickets.Text },
+      { status: StatusCodes.InternalServerError }
+    );
+  }
+}
