7610628d5a96a64af45b70ae3217a82ab42a30df
Simlex <simlexafol@gmail.com>
2025-02-14 08:52:00 +0100

    refactor: simplify AboutPage component by removing unused imports and props

--- app/about/AboutPage.tsx
+++ app/about/AboutPage.tsx
@@ -1,9 +1,6 @@
 "use client"
 import { FunctionComponent, ReactElement } from "react";
-import styles from "@/app/styles/About.module.scss";
-import Image from "next/image";
 import images from "@/public/images";
-import CreateEvent from "../components/Homepage/CreateEvent";
 import PageHeroSection from "../components/shared/PageHeroSection";
 import IntroSection from "../components/AboutPage/IntroSection";
 import WhyChooseUs from "../components/AboutPage/WhyChooseUs";
@@ -16,19 +13,17 @@ interface AboutPageProps {
 }
 
 const AboutPage: FunctionComponent<AboutPageProps> = (): ReactElement => {
-    const appTheme = useSelector((state: RootState) => state.theme.appTheme);
 
     return (
-        <main className={styles.main}>
+        <main>
             <PageHeroSection
                 imageUrl={images.about_hero}
                 title="About Us"
                 description="Ticketsdeck Events ~ where the magic of unforgettable experiences comes to life!"
             />
-            <IntroSection appTheme={appTheme} />
-            <WhyChooseUs appTheme={appTheme} />
+            <IntroSection />
+            <WhyChooseUs />
             <TeamSection />
-            {/* <CreateEvent /> */}
         </main>
     );
 }
