30bf5ea51e8839ba53c281155cb1f4763c5a0238
Simlex <simlexafol@gmail.com>
2025-02-27 17:11:14 +0100

    refactor: extend session and user types to include error handling and token management

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,22 +1,39 @@
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-    /**
-     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-     */
-    interface Session {
-        user: {
-            id: string | null | undefined;
-            name: string | null | undefined;
-            email: string | null | undefined;
-            image: string | null | undefined;
-            username: string | null | undefined;
-            idToken: string | null | undefined;
-            accessToken: string | null | undefined;
-        }
-    }
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    error: string | null | undefined;
+    user: {
+      id: string | null | undefined;
+      name: string | null | undefined;
+      email: string | null | undefined;
+      image: string | null | undefined;
+      username: string | null | undefined;
+      idToken: string | null | undefined;
+      token: string | null | undefined;
+      accessTokenExpires: number;
+    };
+  }
 
-    interface Profile {
-        picture: string | null | undefined;
-    }
+  interface User {
+    id: string | null | undefined;
+    name: string | null | undefined;
+    email: string | null | undefined;
+    image: string | null | undefined;
+    username: string | null | undefined;
+    token: string | null | undefined;
+    refreshToken: string | null | undefined;
+  }
+
+  interface Token extends JWT {
+    id: string;
+    token: string;
+    refreshToken: string;
+    accessTokenExpires: number;
+    error?: string;
+  }
 }
