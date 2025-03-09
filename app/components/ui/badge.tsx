be0e97ea17990144ba8ca71365e4ff3462adf931
Simlex <simlexafol@gmail.com>
2025-03-09 02:54:48 +0100

    feat: create badge component

---
+++ app/components/ui/badge.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {
+    title: string
+    className?: string
+}
+
+export default function Badge({ title, className}: Props) {
+    return (
+        <span className={`mb-4 block w-fit mx-auto p-1 px-3 rounded-2xl text-sm bg-purple-600 hover:bg-purple-700 ${className}`}>{title}</span>
+    )
+}
