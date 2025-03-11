989f2ade9d28680e184e15f952228cee82984fac
Simlex <simlexafol@gmail.com>
2025-03-11 09:24:02 +0100

    feat: add Expand icon component to Icons for enhanced UI functionality

--- app/components/ui/icons.tsx
+++ app/components/ui/icons.tsx
@@ -918,6 +918,12 @@ export const Icons = {
         </svg>
     ),
 
+    Expand: (props: IconProps) => (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+            <path d="M9 9L4 4M4 4V8M4 4H8M15 9L20 4M20 4V8M20 4H16M9 15L4 20M4 20V16M4 20H8M15 15L20 20M20 20V16M20 20H16" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    ),
+
     Like: (props: IconProps) => (
         <svg
             xmlns='http://www.w3.org/2000/svg'
