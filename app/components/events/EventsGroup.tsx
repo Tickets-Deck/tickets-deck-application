5ef5c6fffd9697bcb1fd3f8871a1c3307f33155d
Simlex <simlexafol@gmail.com>
2025-03-13 16:44:03 +0100

    fix: update grid layout in EventsGroup for better responsiveness

--- app/components/events/EventsGroup.tsx
+++ app/components/events/EventsGroup.tsx
@@ -86,7 +86,7 @@ const EventsGroup: FunctionComponent<EventsGroupProps> = ({
                     }`}
             >
                 <div
-                    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
+                    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 min-[1200px]:grid-cols-4 gap-4"
                 // "[grid-template-columns:_repeat(auto-fill,_minmax(200px,_1fr))] min-[550px]:[[grid-template-columns:_repeat(auto-fill,_minmax(250px,_1fr))]]"
                 >
                     {!isFetchingEvents &&
