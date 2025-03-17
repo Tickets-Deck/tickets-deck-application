c198fd620de148929c7175c0e2c922f74ab16dbb
Simlex <simlexafol@gmail.com>
2025-03-17 18:38:09 +0100

    feat: add UserFlag enum for user status management

---
+++ app/enums/UserFlag.ts
@@ -0,0 +1,10 @@
+export enum FlagOptions {
+  isVerified = "isVerified",
+  isBlocked = "isBlocked",
+  isSuspended = "isSuspended",
+  isDeleted = "isDeleted",
+  isSuperAdmin = "isSuperAdmin",
+  isSubscribed = "isSubscribed",
+  isNewsletterSubscribed = "isNewsletterSubscribed",
+  isEmailVerified = "isEmailVerified",
+}
