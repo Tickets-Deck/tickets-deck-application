01b767d9e49a6ff832f41cd9275e6c11c17abb9b
Simlex <simlexafol@gmail.com>
2025-03-29 16:14:53 +0100

    feat: move fetch of order information to server page

--- app/order/[orderId]/OrdersPage.tsx
+++ app/order/[orderId]/OrdersPage.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ReactElement, FunctionComponent, useEffect, useState, useRef } from "react";
+import { ReactElement, FunctionComponent, useEffect, useState, useRef, Suspense } from "react";
 import styles from "@/app/styles/OrdersPage.module.scss";
 import EventMainInfo from "../../components/Event/EventInfo";
 import { EventResponse } from "../../models/IEvents";
@@ -15,16 +15,13 @@ import { Icons } from "@/app/components/ui/icons";
 import { ImagePopup } from "@/app/components/custom/ImagePopup";
 
 interface OrdersPageProps {
-    orderId: string
     hostUrl: string | undefined
+    orderInformation: UserTicketOrderInfo | null
 }
 
-const OrdersPage: FunctionComponent<OrdersPageProps> = ({ orderId, hostUrl }): ReactElement => {
+const OrdersPage: FunctionComponent<OrdersPageProps> = ({ orderInformation, hostUrl }): ReactElement => {
 
-    const fetchOrderInformationById = useFetchOrderInformationById();
-
-    const [isFetchingOrderInformation, setIsFetchingOrderInformation] = useState(true);
-    const [orderInformation, setOrderInformation] = useState<UserTicketOrderInfo | null>(null);
+    const [isFetchingOrderInformation, setIsFetchingOrderInformation] = useState(false);
     const [selectedTicketOrderInfo, setSelectedTicketOrderInfo] = useState<TicketPass>();
     const [isTicketVisible, setIsTicketVisible] = useState(false);
     const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -32,20 +29,20 @@ const OrdersPage: FunctionComponent<OrdersPageProps> = ({ orderId, hostUrl }): R
     /**
      * Function to fetch order information
      */
-    async function handleFetchOrderInformation() {
+    // async function handleFetchOrderInformation() {
 
-        await fetchOrderInformationById(orderId)
-            .then((response) => {
-                setOrderInformation(response.data);
-            })
-            .catch((error) => {
-                setOrderInformation(null);
-            })
-            .finally(() => {
-                // Fetch order information
-                setIsFetchingOrderInformation(false);
-            })
-    };
+    //     await fetchOrderInformationById(orderId)
+    //         .then((response) => {
+    //             setOrderInformation(response.data);
+    //         })
+    //         .catch((error) => {
+    //             setOrderInformation(null);
+    //         })
+    //         .finally(() => {
+    //             // Fetch order information
+    //             setIsFetchingOrderInformation(false);
+    //         })
+    // };
 
     function showTicketUi(ticketOrder: UserTicketOrder) {
         setSelectedTicketOrderInfo({
@@ -56,9 +53,9 @@ const OrdersPage: FunctionComponent<OrdersPageProps> = ({ orderId, hostUrl }): R
         });
     };
 
-    useEffect(() => {
-        handleFetchOrderInformation();
-    }, []);
+    // useEffect(() => {
+    //     handleFetchOrderInformation();
+    // }, []);
 
     useEffect(() => {
         if (selectedTicketOrderInfo) {
@@ -69,102 +66,104 @@ const OrdersPage: FunctionComponent<OrdersPageProps> = ({ orderId, hostUrl }): R
 
     return (
         <>
-            <ModalWrapper
-                visibility={isTicketVisible && (selectedTicketOrderInfo !== undefined)}
-                setVisibility={setIsTicketVisible}
-                styles={{ backgroundColor: 'transparent', color: '#fff', width: "fit-content", overflowY: "auto", maxHeight: "100vh", paddingTop: "50px", paddingBottom: "50px" }}>
+            <Suspense fallback={<div className="bg-black text-white"><p>Loading order information...</p></div>}>
+                <ModalWrapper
+                    visibility={isTicketVisible && (selectedTicketOrderInfo !== undefined)}
+                    setVisibility={setIsTicketVisible}
+                    styles={{ backgroundColor: 'transparent', color: '#fff', width: "fit-content", overflowY: "auto", maxHeight: "100vh", paddingTop: "50px", paddingBottom: "50px" }}>
+                    {
+                        selectedTicketOrderInfo &&
+                        <TicketUi
+                            ticketInfo={selectedTicketOrderInfo}
+                            setIsTicketVisible={setIsTicketVisible}
+                        />
+                    }
+                </ModalWrapper>
                 {
-                    selectedTicketOrderInfo &&
-                    <TicketUi
-                        ticketInfo={selectedTicketOrderInfo}
-                        setIsTicketVisible={setIsTicketVisible}
+                    orderInformation && orderInformation.event &&
+                    <ImagePopup
+                        imageUrl={orderInformation.event.mainImageUrl}
+                        alt={orderInformation.event.title}
+                        isOpen={isPopupOpen}
+                        onClose={() => setIsPopupOpen(false)}
                     />
                 }
-            </ModalWrapper>
-            {
-                orderInformation && orderInformation.event &&
-                <ImagePopup
-                    imageUrl={orderInformation.event.mainImageUrl}
-                    alt={orderInformation.event.title}
-                    isOpen={isPopupOpen}
-                    onClose={() => setIsPopupOpen(false)}
-                />
-            }
 
-            <main className={styles.orderspage}>
-                {
-                    isFetchingOrderInformation && !orderInformation &&
-                    <div className={styles.loaderAreaContainer}>
-                        <div className={styles.loaderArea}>
-                            <ComponentLoader customLoaderColor="#fff" />
+                <main className={styles.orderspage}>
+                    {
+                        isFetchingOrderInformation && !orderInformation &&
+                        <div className={styles.loaderAreaContainer}>
+                            <div className={styles.loaderArea}>
+                                <ComponentLoader customLoaderColor="#fff" />
+                            </div>
+                            <h3>Fetching ticket order information</h3>
+                            <p>Please wait while we fetch your ticket order information.</p>
                         </div>
-                        <h3>Fetching ticket order information</h3>
-                        <p>Please wait while we fetch your ticket order information.</p>
-                    </div>
-                }
+                    }
 
-                {
-                    !isFetchingOrderInformation && !orderInformation &&
-                    <div className={styles.loaderAreaContainer}>
-                        {/* <div className={styles.loaderArea}>
+                    {
+                        !isFetchingOrderInformation && !orderInformation &&
+                        <div className={styles.loaderAreaContainer}>
+                            {/* <div className={styles.loaderArea}>
                         <ComponentLoader customLoaderColor="#fff" />
                     </div> */}
-                        <h3>There was no ticket order found</h3>
-                        <p>It seems the ticket order you are looking for does not exist.</p>
-                    </div>
-                }
-
-                {
-                    !isFetchingOrderInformation && orderInformation &&
-                    <>
-                        <div className={styles.topArea}>
-                            {/* <span className={styles.closeIcon}><ArrowLeftIcon /></span> */}
-                            {/* <h2>Order <span>#{orderInformation?.orderId}</span> on {moment(orderInformation?.createdAt).format("MMM D, YYYY")}</h2> */}
-                            <h2>Order <span>#{orderInformation.orderId}</span></h2>
+                            <h3>There was no ticket order found</h3>
+                            <p>It seems the ticket order you are looking for does not exist.</p>
                         </div>
-                        <div className={styles.orderInformationContainer}>
-                            <div className={styles.eventContainer}>
-                                <EventMainInfo
-                                    eventInfo={orderInformation.event}
-                                    forOrdersPage
-                                    hideStatusTag
-                                    hostUrl={hostUrl}
-                                    setIsPopupOpen={setIsPopupOpen}
-                                />
+                    }
+
+                    {
+                        !isFetchingOrderInformation && orderInformation &&
+                        <>
+                            <div className={styles.topArea}>
+                                {/* <span className={styles.closeIcon}><ArrowLeftIcon /></span> */}
+                                {/* <h2>Order <span>#{orderInformation?.orderId}</span> on {moment(orderInformation?.createdAt).format("MMM D, YYYY")}</h2> */}
+                                <h2>Order <span>#{orderInformation.orderId}</span></h2>
                             </div>
-                            <div className={styles.ticketsContainer}>
-                                <h2>Tickets</h2>
-                                <p className="text-white">Primary email: {orderInformation.contactEmail}</p>
-                                <div className={styles.tickets}>
-                                    {
-                                        orderInformation.orderedTickets.map((orderedTicket, index) => (
-                                            <div className="flex flex-row items-center justify-between py-4 border-b-[1px] border-b-container-grey" key={index}>
-                                                <div className="flex flex-col">
-                                                    <h3>{orderedTicket.ticket.name}</h3>
-                                                    <p className="text-sm italic text-white/50">
-                                                        {orderedTicket.associatedEmail || "Sent to primary email"}
-                                                    </p>
-                                                </div>
-                                                <button
-                                                    className="p-2 px-4 bg-white/10 hover:bg-white/30 rounded-full text-sm transition-all flex flex-row items-center gap-2"
-                                                    onClick={() => showTicketUi(orderedTicket)}
-                                                >
-                                                    <Icons.Ticket width={18} />
-                                                    View Ticket
-                                                </button>
-                                            </div>
-                                        ))
-                                    }
+                            <div className={styles.orderInformationContainer}>
+                                <div className={styles.eventContainer}>
+                                    <EventMainInfo
+                                        eventInfo={orderInformation.event}
+                                        forOrdersPage
+                                        hideStatusTag
+                                        hostUrl={hostUrl}
+                                        setIsPopupOpen={setIsPopupOpen}
+                                    />
                                 </div>
-                                {/* <div className={styles.actions}>
+                                <div className={styles.ticketsContainer}>
+                                    <h2>Tickets</h2>
+                                    <p className="text-white">Primary email: {orderInformation.contactEmail}</p>
+                                    <div className={styles.tickets}>
+                                        {
+                                            orderInformation.orderedTickets.map((orderedTicket, index) => (
+                                                <div className="flex flex-row items-center justify-between py-4 border-b-[1px] border-b-container-grey" key={index}>
+                                                    <div className="flex flex-col">
+                                                        <h3>{orderedTicket.ticket.name}</h3>
+                                                        <p className="text-sm italic text-white/50">
+                                                            {orderedTicket.associatedEmail || "Sent to primary email"}
+                                                        </p>
+                                                    </div>
+                                                    <button
+                                                        className="p-2 px-4 bg-white/10 hover:bg-white/30 rounded-full text-sm transition-all flex flex-row items-center gap-2"
+                                                        onClick={() => showTicketUi(orderedTicket)}
+                                                    >
+                                                        <Icons.Ticket width={18} />
+                                                        View Ticket
+                                                    </button>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                    {/* <div className={styles.actions}>
                         <button>Cancel order</button>
                         <button>Resend tickets to email</button>
                     </div> */}
+                                </div>
                             </div>
-                        </div>
-                    </>
-                }
-            </main>
+                        </>
+                    }
+                </main>
+            </Suspense>
         </>
     );
 }
