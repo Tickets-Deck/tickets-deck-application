21d80d915e28748f3c1e947410aff0f9aa052310
Simlex <simlexafol@gmail.com>
2025-04-05 20:06:08 +0100

    feat: fetch updated user profile after email verification

--- app/components/authPages/VerifyEmailPage.tsx
+++ app/components/authPages/VerifyEmailPage.tsx
@@ -65,6 +65,9 @@ const VerifyEmailPage: FunctionComponent<
 
     await verifyUserEmail(token as string, userId as string)
       .then(() => {
+        // Fetch updated user information
+        dispatch(fetchUserProfile(userId as string));
+
         // Set the isEmailVerified state to true
         setIsEmailVerified(true);
       })
@@ -114,40 +117,40 @@ const VerifyEmailPage: FunctionComponent<
   }, [token, userId]);
 
   // This hook fetches the profile of the user if the user is logged in & the email verified status is true
-  useEffect(() => {
-    if (!user || !isEmailVerified) return;
-    dispatch(fetchUserProfile(session?.user.id as string));
-  }, [isEmailVerified, user]);
+  //   useEffect(() => {
+  //     if (!user || !isEmailVerified) return;
+  //     dispatch(fetchUserProfile(session?.user.id as string));
+  //   }, [isEmailVerified, user]);
 
   return (
-    <div className='max-[768px]:sectionPadding flex md:grid place-items-center py-[5rem] min-h-[90vh] bg-dark-grey'>
+    <div className="max-[768px]:sectionPadding flex md:grid place-items-center py-[5rem] min-h-[90vh] bg-dark-grey">
       <div
         className={`bg-dark-grey-2 text-white rounded-[1.25rem] flex w-screen max-h-none md:mx-auto md:max-h-fit md:w-[70vw] xl:w-[35vw] h-fit overflow-hidden max-w-[450px]`}
       >
         {
           // If there is no token in the search params
           !token && (
-            <div className='py-6 flex px-5 w-full flex-col gap-6'>
-              <div className='flex items-center flex-col gap-1 mb-3'>
-                <h3 className='font-semibold text-xl mb-3'>
+            <div className="py-6 flex px-5 w-full flex-col gap-6">
+              <div className="flex items-center flex-col gap-1 mb-3">
+                <h3 className="font-semibold text-xl mb-3">
                   Verify your email
                 </h3>
-                <p className='text-center text-base w-[80%]'>
+                <p className="text-center text-base w-[80%]">
                   We have sent a verification link to your email address
                   {retrieveNewlyCreatedUserEmail() && (
                     <>
                       &nbsp;
-                      <span className='font-medium text-primary-color-sub underline'>
+                      <span className="font-medium text-primary-color-sub underline">
                         {retrieveNewlyCreatedUserEmail()}
                       </span>
                     </>
                   )}
                 </p>
               </div>
-              <div className='flex flex-col gap-4 md:min-w-[25rem] w-full min-w-[auto]'>
+              <div className="flex flex-col gap-4 md:min-w-[25rem] w-full min-w-[auto]">
                 <button
-                  className='p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80'
-                  type='submit'
+                  className="p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80"
+                  type="submit"
                   onClick={openMailApp}
                   disabled={isVerifyingEmail}
                 >
@@ -160,15 +163,15 @@ const VerifyEmailPage: FunctionComponent<
         {
           // If there is a token in the search params, and the user's email isn't verified yet, and the email is being verified
           token && !isEmailVerified && isVerifyingEmail && (
-            <div className='py-6 flex px-5 w-full flex-col gap-6'>
-              <h3 className='font-semibold text-xl text-center mb-3'>
+            <div className="py-6 flex px-5 w-full flex-col gap-6">
+              <h3 className="font-semibold text-xl text-center mb-3">
                 Verifying your email...
               </h3>
-              <div className='h-[150px] relative grid place-items-center'>
-                <ComponentLoader customLoaderColor='#111111' />
+              <div className="h-[150px] relative grid place-items-center">
+                <ComponentLoader customLoaderColor="#111111" />
               </div>
               {verificationError && (
-                <span className='text-xs text-[#eb485b] flex items-center gap-0.5'>
+                <span className="text-xs text-[#eb485b] flex items-center gap-0.5">
                   {verificationError}
                 </span>
               )}
@@ -178,27 +181,27 @@ const VerifyEmailPage: FunctionComponent<
         {
           // If there is a token in the search params, and the email has been verified successfully
           token && isEmailVerified && !isVerifyingEmail && (
-            <div className='py-6 flex px-5 w-full flex-col items-center gap-6'>
-              <h3 className='font-semibold text-xl mb-3'>Email verified</h3>
+            <div className="py-6 flex px-5 w-full flex-col items-center gap-6">
+              <h3 className="font-semibold text-xl mb-3">Email verified</h3>
               <span>
-                <span className='w-20 h-20 grid place-items-center mx-auto'>
-                  <Icons.Verified className='w-14 h-14 [&_path]:fill-success-color' />
+                <span className="w-20 h-20 grid place-items-center mx-auto">
+                  <Icons.Verified className="w-14 h-14 [&_path]:fill-success-color" />
                 </span>
               </span>
-              <p className='text-center'>
+              <p className="text-center">
                 Your email has been verified successfully.
                 {!user && "You can now login to your account."}
               </p>
               {user ? (
                 <Link
-                  className='w-full p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80'
+                  className="w-full p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80"
                   href={ApplicationRoutes.Home}
                 >
                   Go to homepage
                 </Link>
               ) : (
                 <Link
-                  className='w-full p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80'
+                  className="w-full p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80"
                   href={ApplicationRoutes.SignIn}
                 >
                   Login
@@ -210,8 +213,8 @@ const VerifyEmailPage: FunctionComponent<
         {
           // If there is a token in the search params, and the email verification failed
           token && !isEmailVerified && !isVerifyingEmail && (
-            <div className='py-6 flex px-5 w-full flex-col gap-6 text-center'>
-              <h3 className='font-semibold text-xl mb-3'>
+            <div className="py-6 flex px-5 w-full flex-col gap-6 text-center">
+              <h3 className="font-semibold text-xl mb-3">
                 Failed to verify email
               </h3>
               <p>
@@ -219,8 +222,8 @@ const VerifyEmailPage: FunctionComponent<
                 button below to resend the verification email.
               </p>
               <button
-                className='p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80'
-                type='submit'
+                className="p-[0.65rem] rounded-lg bg-white text-dark-grey text-center cursor-pointer border-none outline-none hover:opacity-80"
+                type="submit"
                 onClick={() => handleResendVerificationLink()}
                 disabled={isResendingEmail}
               >
