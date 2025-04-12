3e1f10676530f87ffeaf84586cc4852ee3a768d3
Simlex <simlexafol@gmail.com>
2025-04-12 23:13:29 +0100

    refactor: modify buildCloudinaryImageUrl function to enhance image URL processing by replacing upload parameters

--- utils/getCloudinaryImageUrl.ts
+++ utils/getCloudinaryImageUrl.ts
@@ -1,3 +1,4 @@
-export const buildCloudinaryImageUrl = (publicId: string) => {
-  return `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/q_auto,f_auto/${publicId}`;
+export const buildCloudinaryImageUrl = (mainImageUrl: string) => {
+  return mainImageUrl.replace("/upload", "/upload/q_auto,f_auto");
+  //   return `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/q_auto,f_auto/${mainImageUrl}`;
 };
