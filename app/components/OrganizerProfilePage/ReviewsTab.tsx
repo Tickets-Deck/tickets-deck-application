8883bfd956b64059fa13ac9eda037904a777370e
Simlex <simlexafol@gmail.com>
2025-04-12 14:52:30 +0100

    feat: refactor ReviewsTab component to improve review fetching and display logic, integrate user session handling, and enhance UI elements

--- app/components/OrganizerProfilePage/ReviewsTab.tsx
+++ app/components/OrganizerProfilePage/ReviewsTab.tsx
@@ -1,220 +1,180 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Icons } from "../ui/icons";
 import Image from "next/image";
 import ReviewModal from "../Modal/ReviewModal";
 import { useCreateReview, useFetchOrganizerReviews } from "@/app/api/apiClient";
 import { useToast } from "@/app/context/ToastCardContext";
-
-export type Review = {
-  id: string;
-  rating: number;
-  reviewText: string;
-  eventAttended: string;
-  eventName: string;
-  reviewerName: string;
-  reviewerImage?: string;
-  date: Date;
-};
-
-// Simple function to format dates relative to now
-function formatRelativeTime(date: Date): string {
-  const now = new Date();
-  const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-
-  if (diffInSeconds < 60) {
-    return "just now";
-  }
-
-  const diffInMinutes = Math.floor(diffInSeconds / 60);
-  if (diffInMinutes < 60) {
-    return `${diffInMinutes} ${diffInMinutes === 1 ? "minute" : "minutes"} ago`;
-  }
-
-  const diffInHours = Math.floor(diffInMinutes / 60);
-  if (diffInHours < 24) {
-    return `${diffInHours} ${diffInHours === 1 ? "hour" : "hours"} ago`;
-  }
-
-  const diffInDays = Math.floor(diffInHours / 24);
-  if (diffInDays < 30) {
-    return `${diffInDays} ${diffInDays === 1 ? "day" : "days"} ago`;
-  }
-
-  const diffInMonths = Math.floor(diffInDays / 30);
-  if (diffInMonths < 12) {
-    return `${diffInMonths} ${diffInMonths === 1 ? "month" : "months"} ago`;
-  }
-
-  const diffInYears = Math.floor(diffInMonths / 12);
-  return `${diffInYears} ${diffInYears === 1 ? "year" : "years"} ago`;
-}
+import Ratings from "../custom/StarRating";
+import { CreateReviewRequest, Review } from "@/app/models/IReview";
+import { formatRelativeTime } from "@/utils/formatRelativeTime";
+import { Session } from "next-auth";
+import {
+  ApplicationContext,
+  ApplicationContextData,
+} from "@/app/context/ApplicationContext";
+import { FullPageLoader } from "../Loader/ComponentLoader";
+import { OrganizerEvents } from "@/app/u/[userId]/OrganizerProfilePage";
+import { ApplicationError } from "@/app/constants/applicationError";
 
 const orientation: "horizontal" | "vertical" = "horizontal";
 
-// Sample initial reviews
-const initialReviews: Review[] = [];
+type ReviewsTabProps = {
+  organizerName: string;
+  organizerId: string;
+  organizerEvents: OrganizerEvents[];
+  session: Session | null;
+};
 
 export function ReviewsTab({
   organizerName,
   organizerEvents,
-  reviewerId,
   organizerId,
-  reviewerToken,
-}: {
-  organizerName: string;
-  reviewerId: string;
-  organizerId: string;
-  reviewerToken: string;
-  organizerEvents: {
-    date: string;
-    title: string;
-    id: string;
-  }[];
-}) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [reviews, setReviews] = useState<Review[]>(initialReviews);
-  const createReview = useCreateReview();
-  const fetchReviews = useFetchOrganizerReviews();
+  session,
+}: ReviewsTabProps) {
+  const fetchOrganizerReviews = useFetchOrganizerReviews();
+  const { toggleUserLoginPrompt } = useContext(
+    ApplicationContext
+  ) as ApplicationContextData;
   const toastHandler = useToast();
 
-  const handleReviewSubmit = async (newReview: {
-    reviewText: string;
-    rating: number;
-    eventId: string;
-  }) => {
-    // In a real app, this would be handled by a server action or API call
-    // Here we're just simulating adding the review to the list
-    const { rating, eventId, reviewText } = newReview;
-    await createReview({
-      rating,
-      eventId,
-      reviewText,
-      reviewerId,
-      organizerId,
-      token: reviewerToken,
-    })
-      .then((response) => {
-        if (!response) return;
-        toastHandler.logSuccess("Success", "Review submitted successfully!");
-        return response.data;
-      })
-      .catch((error) => {
-        if (error.response) throw new Error(error.response.data.message);
-      });
-  };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [reviews, setReviews] = useState<Review[]>();
+  const [isFetchingReviews, setIsFetchingReviews] = useState(true);
 
-  const fetchReviewsData = async () => {
-    await fetchReviews(organizerId)
+  const fetchOrganizerReviewsData = async () => {
+    setIsFetchingReviews(true);
+
+    await fetchOrganizerReviews(organizerId)
       .then((response) => {
         setReviews(response.data);
       })
       .catch((error) => {
         console.error("Error fetching reviews:", error);
+      })
+      .finally(() => {
+        setIsFetchingReviews(false);
       });
   };
 
   useEffect(() => {
-    fetchReviewsData();
+    fetchOrganizerReviewsData();
   }, []);
 
   return (
-    <div className='space-y-6'>
+    <div className="space-y-6">
       {/* Review submission button */}
-      <div className='flex justify-between items-center'>
-        <h2 className='text-xl font-semibold'>Reviews</h2>
-        <button onClick={() => setIsModalOpen(true)} className='tertiaryButton'>
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-semibold">Reviews</h2>
+        <button
+          onClick={() =>
+            session?.user ? setIsModalOpen(true) : toggleUserLoginPrompt()
+          }
+          className="tertiaryButton"
+        >
           Write a Review
         </button>
       </div>
 
       {/* Reviews list */}
-      {reviews.length > 0 ? (
-        <div className='space-y-6'>
+      {isFetchingReviews ? (
+        <div className="text-center py-12 bg-white/0 rounded-lg">
+          <FullPageLoader containerClassName="!h-[5rem]" />
+          <p className="text-white/80">Fetching reviews</p>
+        </div>
+      ) : reviews && reviews.length > 0 ? (
+        <div className="space-y-6">
           {/* Review stats */}
-          <div className='bg-muted/30 rounded-lg p-4 flex flex-col md:flex-row gap-6 items-center justify-between'>
-            <div className='flex items-center gap-4'>
-              <div className='flex flex-col items-center'>
-                <span className='text-3xl font-bold'>
+          <div className="bg-container-grey-20/30 rounded-lg p-4 px-5 flex flex-col md:flex-row gap-6 items-center justify-between">
+            <div className="flex items-center gap-6 w-full justify-between md:w-fit">
+              <div className="flex flex-col items-center">
+                <span className="text-3xl font-bold font-Mona-Sans-Wide">
                   {(
                     reviews.reduce((acc, review) => acc + review.rating, 0) /
                     reviews.length
                   ).toFixed(1)}
                 </span>
-                <div className='flex'>
-                  {[1, 2, 3, 4, 5].map((star) => (
-                    <Icons.Star
-                      key={star}
-                      className={`h-4 w-4 ${
-                        star <=
-                        Math.round(
-                          reviews.reduce(
-                            (acc, review) => acc + review.rating,
-                            0
-                          ) / reviews.length
-                        )
-                          ? "fill-primary-color text-primary-color"
-                          : "text-white"
-                      }`}
-                    />
-                  ))}
+                <div className="flex flex-row items-end">
+                  {[1, 2, 3, 4, 5].map((star) => {
+                    return star <=
+                      Math.round(
+                        reviews.reduce(
+                          (acc, review) => acc + review.rating,
+                          0
+                        ) / reviews.length
+                      ) ? (
+                      <Icons.Star key={star} className={`h-4 w-4`} />
+                    ) : (
+                      <Icons.Star
+                        key={star}
+                        className={`h-4 w-4`}
+                        fill="#8a8a8a"
+                      />
+                    );
+                  })}
                 </div>
-                <span className='text-sm text-white mt-1'>
+                <span className="text-sm text-white mt-1">
                   {reviews.length} {reviews.length === 1 ? "review" : "reviews"}
                 </span>
               </div>
-              <div className='h-12 w-px bg-border hidden md:block'></div>
-              <div className='space-y-1'>
+              <div className="h-12 w-px bg-border hidden md:block"></div>
+              <div className="space-y-1">
                 {[5, 4, 3, 2, 1].map((rating) => {
                   const count = reviews.filter(
                     (review) => review.rating === rating
                   ).length;
                   const percentage = (count / reviews.length) * 100;
                   return (
-                    <div key={rating} className='flex items-center gap-2'>
-                      <span className='text-sm w-3'>{rating}</span>
-                      <div className='h-2 bg-muted w-32 rounded-full overflow-hidden'>
+                    <div key={rating} className="flex items-center gap-2">
+                      <span className="text-sm w-3">{rating}</span>
+                      <div className="h-2 bg-muted w-32 rounded-full overflow-hidden">
                         <div
-                          className='h-full bg-primary-color rounded-full'
+                          className="h-full bg-primary-color rounded-full"
                           style={{ width: `${percentage}%` }}
                         ></div>
                       </div>
-                      <span className='text-xs text-white'>{count}</span>
+                      <span className="text-xs text-white">{count}</span>
                     </div>
                   );
                 })}
               </div>
             </div>
-            <div className='text-center md:text-right'>
-              <p className='text-sm text-white mb-2'>
+            <div className="flex flex-col items-end text-center md:text-right">
+              <p className="text-sm text-white mb-2">
                 Was this organizer helpful?
               </p>
-              <button className='hover:border-primary-color/50 hover:text-primary-color'>
-                <Icons.ThumbsUp className='h-4 w-4 mr-2' />
+              <button className="primaryButton">
+                <Icons.ThumbsUp className="h-4 w-4 mr-2" />
                 Recommend
               </button>
             </div>
           </div>
 
           {/* Individual reviews */}
-          <div className='space-y-6'>
+          <div className="space-y-6">
             {reviews.map((review) => (
-              <div key={review.id} className='space-y-4'>
-                <div className='flex justify-between'>
-                  <div className='flex items-center gap-3'>
-                    <div className='h-32 w-32 border-4 border-text-grey relative rounded-full overflow-hidden'>
+              <div key={review.id} className="space-y-4">
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-3">
+                    <div className="h-12 w-12 border-2 border-white relative rounded-full overflow-hidden">
                       <Image
-                        src='https://placehold.co/300x300/8133F1/FFFFFF/png?text=SA'
-                        alt='Similoluwa Afolabi'
+                        src={
+                          review.reviewerProfilePhoto ??
+                          "https://placehold.co/300x300/8133F1/FFFFFF/png?text=SA"
+                        }
+                        alt="Similoluwa Afolabi"
                         fill
-                        sizes='auto'
+                        sizes="auto"
+                        className="sizes-full object-cover"
                       />
                     </div>
                     <div>
-                      <div className='font-medium'>{review.reviewerName}</div>
-                      <div className='text-sm text-white'>
-                        {formatRelativeTime(review.date)}
+                      <div className="font-medium capitalize">
+                        {review.reviewerName}
+                      </div>
+                      <div className="text-sm text-white/80">
+                        {formatRelativeTime(new Date(review.createdAt))}
                       </div>
                     </div>
                   </div>
@@ -235,27 +195,16 @@ export function ReviewsTab({
                 </div>
 
                 <div>
-                  <div className='flex items-center gap-1 mb-1'>
-                    {[1, 2, 3, 4, 5].map((star) => (
-                      <Icons.Star
-                        key={star}
-                        className={`h-4 w-4 ${
-                          star <= review.rating
-                            ? "fill-primary-color text-primary-color"
-                            : "text-white"
-                        }`}
-                      />
-                    ))}
-                  </div>
-                  <div className='text-sm text-white mb-2'>
+                  <Ratings rating={review.rating} />
+                  <div className="text-sm text-white/70 mb-2 capitalize">
                     Event: {review.eventName}
                   </div>
-                  <p className='text-sm'>{review.reviewText}</p>
+                  <p className="text-sm">{review.reviewText}</p>
                 </div>
 
-                <div className='flex items-center gap-4'>
-                  <button className='h-8 hover:text-primary-color'>
-                    <Icons.ThumbsUp className='h-3 w-3 mr-1' />
+                <div className="flex items-center gap-4">
+                  <button className="primaryButton !p-0 !bg-transparent hover:text-gray-400">
+                    <Icons.ThumbsUp className="h-3 w-3 mr-1" />
                     Helpful
                   </button>
                 </div>
@@ -273,9 +222,9 @@ export function ReviewsTab({
           </div>
         </div>
       ) : (
-        <div className='text-center py-12 bg-white/10 rounded-lg'>
-          <h3 className='text-lg font-medium mb-2'>No Reviews Yet</h3>
-          <p className='text-white/80'>
+        <div className="text-center py-12 bg-white/10 rounded-lg">
+          <h3 className="text-lg font-medium mb-2">No Reviews Yet</h3>
+          <p className="text-white/80">
             Be the first to leave a review for this organizer
           </p>
         </div>
@@ -283,10 +232,13 @@ export function ReviewsTab({
 
       <ReviewModal
         events={organizerEvents}
-        handleReviewSubmit={handleReviewSubmit}
+        fetchOrganizerReviewsData={fetchOrganizerReviewsData}
+        token={session?.user.token as string}
         modalVisibility={isModalOpen}
         setModalVisibility={setIsModalOpen}
         organizerName={organizerName}
+        reviewerId={session?.user.id as string}
+        organizerId={organizerId}
       />
     </div>
   );
