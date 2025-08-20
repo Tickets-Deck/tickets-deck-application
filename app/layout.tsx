45d7ed79929e00c33c59e24ca51dbe01599f6bf3
Simlex <simlexafol@gmail.com>
2025-08-20 20:26:13 +0100

    fix: reorder script tags for Google Tag Manager integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -166,23 +166,24 @@ export default async function RootLayout({
               alt="Facebook Pixel"
             />
           </noscript>
-        </head>
-        <body>
-          <Layout children={children} session={session} userData={userData} />
-        </body>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
           
             gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
           `}
-        </Script>
+          </Script>
+        </head>
+        <body>
+          <Layout children={children} session={session} userData={userData} />
+        </body>
       </html>
     </GlobalProvider>
   );
