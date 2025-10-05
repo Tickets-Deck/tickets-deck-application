3e19cd37e56ddf5de0753d382aa119dd4d777aee
Simlex <simlexafol@gmail.com>
2025-10-05 13:51:45 +0100

    feat: implement Homepage component with event fetching and user session handling

--- app/signup.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement, FunctionComponent } from "react";
-
-interface SignupProps {}
-
-const Signup: FunctionComponent<SignupProps> = (): ReactElement => {
-  return <div className=''></div>;
-};
-
-export default Signup;
